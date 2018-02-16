7ef1a1900c9de9d5760cf2da6b45746dda5d2dff
Tatiane Micheletti <tmichele@localhost.localdomain>
2018-02-16 09:33:02 -0800

    Testing raster :: still doesn't work

--- loadData.R
+++ loadData.R
@@ -16,16 +16,7 @@ nochangeOriginal <- raster(file.path(getwd(),"data/C2C_Change_NoChange/C2C_chang
 #Crop the national map to the shapefile
 #Cache(mask, x = yearOriginal, mask = testArea, filename="C2C_year.tif") #Too slow
 
-# FAILS: Returns error
  cutline <- as.character(file.path(getwd(),"data/testArea/testArea.shp"))
- Cache(gdalwarp, srcfile = "C2C_change_year.tif", dstfile = "C2C_year.tif",
-         s_srs = as.character(crs(yearOriginal)), t_srs = as.character(crs(yearOriginal)),
-         extent(yearOriginal), tr = res(yearOriginal),
-         dstalpha = TRUE, multi = TRUE, of = "GTiff", cutline = cutline,
-         crop_to_cutline = TRUE, overwrite = TRUE,
-         output_Raster = TRUE)
-
-# FAILS: Returns NULL
  year <- "C2C_year.tif"
  
 # ----- WITH CACHE -------- NOT WORKING, not creating the file
@@ -45,8 +36,14 @@ dstalpha = TRUE,
 s_srs= as.character(crs(yearOriginal)),
 t_srs= as.character(crs(yearOriginal)),
 multi=TRUE, of="GTiff",
-crop_to_cutline = TRUE, tr=c(250, 250),
+crop_to_cutline = TRUE, tr=c(30, 30),
 filename(yearOriginal), ot = "CFloat64",
 year)
- 
-testArea.ras <- raster(file.path(getwd(),"cache/C2C_year.tif"))
+
+testArea.ras <- raster::stack(file.path(getwd(),"C2C_year.tif"))
+
+testArea.ras <- setMinMax(testArea.ras)
+tA1 <- raster(testArea.ras$C2C_year.1)
+tA1 <- setMinMax(tA1)
+tA2 <- raster(testArea.ras$C2C_year.2)
+tA2 <- setMinMax(tA2)
