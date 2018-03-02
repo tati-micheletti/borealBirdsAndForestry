e6bfc14fea518a02f1322e2fa46751be43ef8379
tati-micheletti <tati.micheletti@gmail.com>
2018-03-02 11:50:51 -0800

    function extractData::working copied from BCWarblers and implemented for possible lists of rasters

--- modules/birdDensityBCR_Prov_LCC/R/extractData.R
+++ modules/birdDensityBCR_Prov_LCC/R/extractData.R
@@ -1,12 +1,33 @@
 # Extracting the values from the rasters to feed the bird predictions
 
-extractData <- function(dataset.raster){
-  data <- data.frame(coordinates(dataset.raster),
-                     raster::values(dataset.raster))
-  names(data) <- c("X", "Y", "Value")  
+extractData <- function(datasetRaster, typeData, list){
+  
+  require(data.table)
+  require(raster)
+  
+  if (typeData == "list") {
+    
+    data <- rep(list(NA),length(list))
+    
+    for (i in 1:length(list)){
 
-  ras <- data
+      names(data)[i] <- list[i]
+      
+        data[[paste0(list[i])]] <- data.table(sp::coordinates(datasetRaster[[paste0(list[i])]]),
+                                              raster::values(datasetRaster[[i]]))
+      names(data[[paste0(list[i])]]) <- c("X", "Y", "Value")
+      
+    }
 
-return(ras)
+  return(data)
+  
+  } else {
+    
+      data <- data.table(coordinates(datasetRaster),
+                         raster::values(datasetRaster))
+      names(data) <- c("X", "Y", "Value")  
+      
+      return(data)
+  }
 
 }
