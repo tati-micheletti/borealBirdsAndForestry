9c77f3f571f4b9ba9db9767afd5edb190192e38f
tati-micheletti <tati.micheletti@gmail.com>
2018-03-02 11:51:45 -0800

    function fetchData::working generalized the list based on input from user in global script.

--- modules/birdDensityBCR_Prov_LCC/R/fetchData.R
+++ modules/birdDensityBCR_Prov_LCC/R/fetchData.R
@@ -1,29 +1,24 @@
-fetchData <- function(birdSp) {
+fetchData <- function(sim, birdSp) {
   
-  dataZipList <- list(BBWA = BBWA,
-                      BLPW = BLPW,
-                      BOCH = BOCH,
-                      BRCR = BRCR,
-                      BTNW = BTNW,
-                      CAWA = CAWA,
-                      CMWA = CMWA,
-                      CONW = CONW,
-                      OVEN = OVEN,
-                      PISI = PISI,
-                      RBNU = RBNU,
-                      SWTH = SWTH,
-                      TEWA = TEWA,
-                      WETA = WETA,
-                      YRWA = YRWA)
+  require(raster)
+  
+  dataRaster <- vector(mode = "list", length = length(birdSp))
+  names(dataRaster) <- as.list(birdSp)
   
   for (i in 1:length(birdSp)) {
 
+    dataDir <- file.path(modulePath(sim),"birdDensityBCR_Prov_LCC/data")
+    
+    if(!paste0(birdSp[i],"_currmean.asc") %in% list.files(dataDir)){
     temp <- tempfile()
-    dataDir <- file.path(getwd(),"modules/birdDensityBCR_Prov_LCC/data")
     download.file(paste0("https://s3-us-west-2.amazonaws.com/bam-databasin-climatechangepredictions/climatepredictions-ascii/",
-                         birdSp[i],"_current.zip",temp))
+                         birdSp[i],"_current.zip"),destfile = temp, cacheOK = TRUE)
     unzip(temp, exdir = dataDir)
-    dataZipList[[paste0(birdSp[i])]] <- raster::raster(file.path(paste0(dataDir,birdSp,".asc")))
+    }
+    
+    dataRaster[[paste0(birdSp[i])]] <- raster::raster(file.path(dataDir,paste0(birdSp[i],"_currmean.asc")))
   }
-  return(dataZipList)
+  return(dataRaster)
+  
   }
+
