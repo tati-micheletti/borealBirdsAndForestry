0c6d04234415b90409435c13b221a76cd5736d0f
tati-micheletti <tati.micheletti@gmail.com>
2018-03-02 13:21:57 -0800

    Single module birdDensityBCR global script Another global option

---
+++ global2.R
@@ -0,0 +1,48 @@
+# Global script for birdForestry project
+
+library(SpaDES)
+
+# set the directories
+workDirectory <- getwd()
+
+paths <- list(
+  cachePath = file.path(workDirectory, "cache"),
+  modulePath = file.path(workDirectory, "modules"),
+  inputPath = file.path(workDirectory, "inputs"),
+  outputPath = file.path(workDirectory, "outputs")
+)
+
+setPaths(modulePath = paths$modulePath, inputPath = paths$inputPath, outputPath = paths$outputPath, cachePath = paths$cachePath)
+
+## list the modules to use
+#modules <- list("glmerBirdModels") #birdDensityBCR_Prov_LCC
+modules <- list("birdDensityBCR_Prov_LCC") #
+
+## Set simulation and module parameters
+
+times <- list(start = 1985, end = 1985, timeunit = "year")
+parameters <- list(birdDensityBCR_Prov_LCC = list(start = 1985, end = 1985)
+)
+
+# objects = list(studyArea = "testArea.shp",
+# #               species = c("PISI","UEFL","YRWA","DEJU"), #Once this works without species, turn on species selection
+#                typeDisturbance = c("Transitional", "Permanent", "Both", "Undisturbed"),
+#                disturbanceDimension = c("local", "neighborhood"))
+
+objects = list(birdSpecies = c("PISI", "BLPW"))
+# birdSpecies <- c("BBWA", "BLPW", "BOCH", "BRCR", 
+#                  "BTNW", "CAWA", "CMWA", "CONW", 
+#                  "OVEN", "PISI", "RBNU", "SWTH", 
+#                  "TEWA", "WETA", "YRWA")
+
+# dev.useRSGD(TRUE) # do not use Rstudio graphics device
+# dev() # opens external (non-RStudio) device, which is faster
+# clearPlot()
+
+## Simulation setup
+mySim <- simInit(times = times, params = parameters, modules = modules, paths =  paths, objects = objects)
+mySimOut <- spades(mySim, debug = TRUE) #c("warblersPointCountBC","init")
+
+moduleDiagram(mySim)
+objectDiagram(mySim)
+
