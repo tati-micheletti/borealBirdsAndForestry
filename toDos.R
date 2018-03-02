8c84e3c361025cdfde54e17e2420a0c4d942aa75
tati-micheletti <tati.micheletti@gmail.com>
2018-03-02 15:55:58 -0800

    To DO's List Next tasks

---
+++ toDos.R
@@ -0,0 +1,8 @@
+# TO DO's
+
+# 1. Check models for convergence. If they don't converge, re-run them without the interaction Cluster:Year [OK] / Fix this module for the data from a table
+# 2. Make a module for retrieving Peter's offsets from QPAD for the species and localtions? Check how QPAD works
+# 3. Make a module to either get info on proportion of disturbance from an "excel file" or from a module (this would be to either feed glmerBirdModels or predictModule)
+# 4. Make a module that will put all data together
+# 5. FIx log(birdDensityBCR_Prov_LCC)
+# 6. Module for dataUploading (abundance, X, Y, Cluster, YYYY)
