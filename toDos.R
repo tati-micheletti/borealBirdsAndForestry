32068bba655b1d71771b78aa453df12e2c56fd8b
tati-micheletti <tati.micheletti@gmail.com>
2018-03-06 10:28:11 -0800

    To do's

--- toDos.R
+++ toDos.R
@@ -1,8 +1,18 @@
 # TO DO's
 
 # 1. Check models for convergence. If they don't converge, re-run them without the interaction Cluster:Year [OK] / Fix this module for the data from a table
-# 2. Make a module for retrieving Peter's offsets from QPAD for the species and localtions? Check how QPAD works
-# 3. Make a module to either get info on proportion of disturbance from an "excel file" or from a module (this would be to either feed glmerBirdModels or predictModule)
+# 2. Make a module for retrieving Peter's offsets from QPAD for the species and localtions? Check how QPAD works (function load_BAM_QPAD()?)
+    # Already installed QPAD, and uploaded the offsets. Just don't know how to use them! There is a .RData file in BAM Gdrive (pack_2016-12-01.RData),
+#     but I wanted something coming from the QPAD. If not possible, ask Nicole if she can upload this file to BAM's website.
+# 3. Make a module to either get info on proportion of disturbance from an "excel file" or from a module (this would be to either 
+#     feed glmerBirdModels or predictModule). Already started: module getDisturbanceData, but didn't write much yet.
 # 4. Make a module that will put all data together
-# 5. FIx log(birdDensityBCR_Prov_LCC)
+# 5. Fix log(birdDensityBCR_Prov_LCC)
 # 6. Module for dataUploading (abundance, X, Y, Cluster, YYYY)
+
+
+# TO DO's Alberto's work:
+# 1. Double check dataset for duplicates;
+# 2. Run the models in SpaDES with the whole dataset for all species;
+# 3. Redo the graphs / figures;
+# 4. Correct the text
