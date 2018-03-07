64467c1afd25766f4334a9805c296364dcc53861
tati-micheletti <tati.micheletti@gmail.com>
2018-03-07 10:26:46 -0800

    loadCroppedData :: still needs testing for inexistent file to check if it is downloading properly

---
+++ modules/glmerBirdModels/R/loadCroppedData.R
@@ -0,0 +1,38 @@
+# loadData Function
+
+#CROPS (IF NECESSARY) AND LOAD DATA
+
+loadCroppedData <- function(data){
+  
+  require(data.table)
+  require(raster)
+  require(sf)
+  
+  if (params(sim)$glmerBirdModels$cropForModel==TRUE){
+    
+    # data.path <- file.path(getwd(), "modules/glmerBirdModels/data", data)
+    
+    if (is.null("birdData")){ # Still to test for automatic download of data. If it doesn't work: try exists(data.path) and uncommenting line above
+      invisible(readline(prompt="Make dure you have the dataset in Google Drives folder 'BAM', and press [enter] to continue"))
+      require(googledrive)
+      drive_download(file.path("BAM",data), path = file.path(getwd(), "modules/glmerBirdModels/data", data), overwrite = FALSE,verbose = FALSE)
+      birdData <- fread(file.path(getwd(), "modules/glmerBirdModels/data", data))
+    }
+    
+    studyArea <- sf::st_transform(x = sim$studyArea, crs = "+init=epsg:4326")
+    
+    xmin <- raster::extent(studyArea)[1]
+    xmax <- raster::extent(studyArea)[2]
+    ymin <- raster::extent(studyArea)[3]
+    ymax <- raster::extent(studyArea)[4]
+    
+    birdData <- birdData[birdData$X>xmin & birdData$X<xmax &
+                   birdData$Y>ymin & birdData$Y<ymax,]
+    
+    if (nrow(birdData)==0){
+      stop("The selected area doesn't contain data. Try increasing the area.")
+    }
+  }
+  
+  return(birdData)
+}
