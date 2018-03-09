1a6c423893ba12221ff3935527122362c2346582
Unknown <tati.micheletti@gmail.com>
2018-03-09 07:18:12 -0800

    Trying to sort out plotResults.R

--- plotResults.R
+++ plotResults.R
@@ -11,13 +11,37 @@ plotDisturbanceSector <- function(dataset, typeDisturbance, disturbanceDimension
   require(ggplot2)
   require(reproducible)
   
-  plotDist <- list()
-  
-  plotDist <- lapply(X = dataset, FUN = function(typeDisturbance, disturbanceDimension){
+  #plotDist <- list()
+  plotDist <- lapply(X = dataset, FUN = function(x){
     
-    # FROM HERE!!!
+    browser()
+    
+    listName <- names(dataset[x])
+    
     
   })
+    
+    
+    {
+    
+    listName <- names(dataset[x])
+    state <- ifelse(grepl(listName, "local"),"100","500")
+
+      DT <- subset(x, eval(parse(text = paste0("State_P_",state)))>0)
+      legend <- factor(eval(parse(text = paste0("DT$", "Agent_", state))))
+      
+      graph <- qplot(eval(parse(text = paste0("DT$","State_P_",state))), 
+                    geom="histogram", 
+                    fill=legend, 
+                    xlab="Proportion of disturbed area, local scale", 
+                    ylab="Number of surveys") +
+                theme(axis.text=element_text(size=12), 
+                      axis.title=element_text(size=16,face="bold"),
+                      legend.text=element_text(size=12), 
+                      legend.title=element_text(size=14))
+      return(graph)
+  })
+  
   
   for (dD in disturbanceDimension){
     for (tD in typeDisturbance){
