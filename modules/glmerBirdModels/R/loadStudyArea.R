97733fa38397ca2b89c15b02dfc1933e1afd870a
Unknown <tati.micheletti@gmail.com>
2018-03-10 10:32:07 -0800

    Correction of the function

--- modules/glmerBirdModels/R/loadStudyArea.R
+++ modules/glmerBirdModels/R/loadStudyArea.R
@@ -5,7 +5,7 @@ loadStudyArea <-function(data){
   require(rgdal)
   require(sf)
   
-  studyArea <- sf::st_read(dsn = file.path(getwd(),"data", "testArea.shp"))
+  studyArea <- sf::st_read(dsn = file.path(getwd(),"data", data))
   
   return(studyArea)
 }
