c64149a67868902d94c28004be1b613ae97e8426
tati-micheletti <tati.micheletti@gmail.com>
2018-03-09 20:07:55 -0800

    updated birdModelFunctions::faster and elegant

--- modules/glmerBirdModels/R/birdModelsFunction.R
+++ modules/glmerBirdModels/R/birdModelsFunction.R
@@ -1,68 +1,46 @@
-birdModelsFunction <- function(sim, disturbanceDimension, typeDisturbance, birdSp){
-  
-# disturbanceDimension <- c("local", "neighborhood)
-# typeDisturbance <- c("Transitional", "Permanent", "Undisturbed", "Both")
-#  For undisturbed, it uses neighborhood model, with local==0
+# Bird model functions version 2
+
+birdModelsFunction <- function(dataset, birdSp){
   
   require(lme4)
   
   models <- list()
   modelsList <- list()
   
-  for (dd in 1:length(disturbanceDimension)){
-    for (td in 1:length(typeDisturbance)){
-      
-      
-      if (disturbanceDimension[dd]=="local"){
-    dimension <- paste0("State_P_100")
-  }
-    
-  if (disturbanceDimension[dd]=="neighborhood"){
-    dimension <- paste0("State_P_500")
-  }
+  models <- lapply(X = combinations, FUN = function(x){
+                    dimension <- ifelse(grepl("local", x),"State_P_100","State_P_500")
+                    data <- dataset[[x]]
+                    
+                for (name in birdSp){
+                      
+                      suppressMessages(assign(name, eval(parse(text = paste0("glmer(AB_", name, " ~ get(dimension) + LOG_BCR_", name, " + ", 
+                                                                             "offset(OF_", name,") + (1|ClusterSP) + (1|YYYY) + (1|ClusterSP:YYYY)",
+                                                                             ", family='poisson', data=data)")))))
+                      
+                      if (!is.null(eval(parse(text = paste0(name,'@optinfo$conv$lme4$messages'))))){
+                        
+                        suppressMessages(assign(name, eval(parse(text = paste0("glmer(AB_", name, " ~ get(dimension) + LOG_BCR_", name, " + ",
+                                                                               "offset(OF_", name,") + (1|ClusterSP) + (1|YYYY)",
+                                                                               ", family='poisson', data=data)")))))
+                      }
+                      
+                      if (!is.null(eval(parse(text = paste0(name,'@optinfo$conv$lme4$messages'))))){
+                        assign(name,paste(as.character("Bad, bad model. No donut for you! Convergence failed.", 
+                                                       "Try re-running the model with less random effects."), sep = " "))}
+                      
+                      modelsList[[name]] <- get(name)
+                      models[[x]] <- modelsList
+                }
 
-    if (typeDisturbance[td]=="Both"){
-
-      data <- sim$data$fullData
+            return(models)
+  })
   
-      }  else
-  
-      if (disturbanceDimension[dd]=="neighborhood"&typeDisturbance[td]=="Undisturbed"){
-        
-        next
-
-        } else {
-        
-      data <- sim$data[[paste0(disturbanceDimension[dd],typeDisturbance[td])]]
-      
-      }
-    
-  ##### MODELS #####
-      
-      for (name in birdSp){
-      
-        suppressMessages(assign(name, eval(parse(text = paste0("glmer(AB_", name, " ~ get(dimension) + LOG_BCR_", name, " + ", 
-                                              "offset(OF_", name,") + (1|ClusterSP) + (1|YYYY) + (1|ClusterSP:YYYY)",
-                                              ", family='poisson', data=data)")))))
-        
-      if (!is.null(eval(parse(text = paste0(name,'@optinfo$conv$lme4$messages'))))){
-
-        suppressMessages(assign(name, eval(parse(text = paste0("glmer(AB_", name, " ~ get(dimension) + LOG_BCR_", name, " + ",
-                                              "offset(OF_", name,") + (1|ClusterSP) + (1|YYYY)",
-                                              ", family='poisson', data=data)")))))
-      }
-        
-        if (!is.null(eval(parse(text = paste0(name,'@optinfo$conv$lme4$messages'))))){
-          assign(name,paste(as.character("Bad, bad model. No donut for you! Convergence failed. Try re-running the model with less random effects.")))}
-          
-        modelsList[[name]] <- get(name)
-      
-      }
-      
-      models[[paste0(disturbanceDimension[dd],typeDisturbance[td])]] <- modelsList
-      
-    }
+  l.models <- list()
+  for (i in 1:length(models)){
+    l.models[i] <- models[[i]]
   }
+  names(l.models) <- combinations
   
-  return(models)
+  return(l.models)
+  
 }
