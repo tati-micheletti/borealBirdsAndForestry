cc754a7ce4653811af8e070e10c5811346f2e497
tati-micheletti <tati.micheletti@gmail.com>
2018-03-12 15:38:34 -0700

    birdModelFunction() :: working with function to exclude outliers. Message warning it not working.

--- modules/glmerBirdModels/R/birdModelsFunction.R
+++ modules/glmerBirdModels/R/birdModelsFunction.R
@@ -6,7 +6,7 @@ birdModelsFunction <- function(combinations, dataset, birdSp){
   
   models <- list()
   modelsList <- list()
-  
+  errorModels <- list()
   
   models <- lapply(X = combinations, FUN = function(x){
     dimension <- ifelse(grepl("local", x),"State_P_100","State_P_500")
@@ -29,24 +29,24 @@ birdModelsFunction <- function(combinations, dataset, birdSp){
         
         if (!is.null(eval(parse(text = paste0(name,'@optinfo$conv$lme4$messages'))))){
           assign(name,paste(as.character("Bad, bad model. No donut for you! Convergence failed.", 
-                                       "Try re-running the model with less random effects."), sep = " "))
+                                         "Try re-running the model with less random effects."), sep = " "))
+        }
+        
+        modelsList[[name]] <- get(name)
+        models[[x]] <- modelsList
+        return(models)
+        
+      }, error = function(e){
+        
+        # phrase <- c("WARNING: Offset outliers excluded.")
+        # errorModels[[x]][[name]] <<- phrase # Not working...
+        models[[x]] <- dropOutliers(x, dataset, name, probs = c(0.20, 0.80))
+        return(models)
       }
-      
-      modelsList[[name]] <- get(name)
-      models[[x]] <- modelsList
-      return(models)
-      
-    }, error = function(e){
-      
-      phrase <- c("ERROR: Try rescaling offsets or variables.")
-      modelsList[[name]] <- phrase
-      models[[x]] <- modelsList
-      return(models)
-    }
-  ) # End tryCatch
+      ) # End tryCatch
       
     })
-  
+    
   })
   
   l.models <- list()
@@ -62,5 +62,5 @@ birdModelsFunction <- function(combinations, dataset, birdSp){
   names(l.models2) <- combinations
   
   return(l.models2)
-
+  
 }
