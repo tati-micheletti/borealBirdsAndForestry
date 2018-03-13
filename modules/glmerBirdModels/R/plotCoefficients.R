96cd1df57d09c7d7b25a870352d53725a38df28f
tati-micheletti <tati.micheletti@gmail.com>
2018-03-13 15:09:41 -0700

    plotCoefficients() :: ready

--- modules/glmerBirdModels/R/plotCoefficients.R
+++ modules/glmerBirdModels/R/plotCoefficients.R
@@ -1,70 +1,63 @@
-#--------------------- DATA TEST ------------------------#
-
-models <- readRDS(file.path(getwd(),"modelsTrialforGraphs2.rds"))
-
-mod1 <- models[[1]][[1]][[1]][[1]]
-mod2 <- models[[1]][[2]][[1]][[1]]
-
-mod3 <- models[[2]][[1]][[1]][[1]]
-mod4 <- models[[2]][[2]][[1]][[1]]
-
-localTransitional <- list(BBWA = mod1, CONW = mod2)
-neighborhoodTransitional <- list(BBWA = mod3, CONW = mod4)
-
-models <- list(localTransitional = localTransitional, neighborhoodTransitional = neighborhoodTransitional)
-
-#----------------------------------------------------------
 
 plotCoefficients <- function(dataset, combinations = sim$combinations, species = sim$birdSpecies){
-  
+
   require(lmerTest)
   require(data.table)
-  
+  require(ggplot2)
+  require(ggalt)
+
   coefTable <- lapply(X = combinations, FUN = function(x){
-    
+
     birdSpecies <- lapply(X = species, FUN = function(name){
       
       isolatedModel <- eval(parse(text = paste0("dataset[[x]]$",name)))
       coef <- as.data.frame(summary(isolatedModel)$coefficients, keep.rownames = TRUE)
-      estimate <- coef["get(dimension)",][1:2]
+      estimate <- coef["get(dimension)",][c(1:2, 4)]
+      colnames(estimate) <- c("Estimate", "Std.Error","p")
       rownames(estimate) <- name
-      estimate$lowerCI <- estimate$Estimate - estimate$`Std. Error`
-      estimate$upperCI <- estimate$Estimate + estimate$`Std. Error`
+      estimate$lowerCI <- estimate$Estimate - estimate$`Std.Error`
+      estimate$upperCI <- estimate$Estimate + estimate$`Std.Error`
+
       return(estimate)
     })
 
     birdSpeciesUnlisted <- do.call(rbind, unname(birdSpecies))
-
     return(birdSpeciesUnlisted)
-    
-  })
-  
-  names(coefTable) <- combinations
-  
-  plotList <- lapply(X = combinations, FUN = function(x){
-    
-    
-    ## RETHINK HERE! To make a facet_grid, I need to use variables as All disturbances, 
-    # Permanent and Transitional as Columns and Transitional, Permanent and Both as rows
-    # The one below is probably not the best approach! Don't forget to add SIGNIFICANCY of MODELS 
-    # as TRUE/FALSE!
-    
-  tempPlot <- ggplot() +
-                      facet_grid()
-      
-      # plot(x, 1:length(x), pch=ifelse(i==1, 19, ifelse(i==2, 8, 21)), xlab="Local scale effects", ylab= "Species", xlim=c(-2,0.5), yaxt="n", main="")
-      # segments(c1, 1:length(x), c2, 1:length(x))
-      # text(c1-0.2, 1:length(x), species_names, cex=0.9)
-      # abline(h=0, v=0, lty=2)
-  
-      return(tempPlot)
-  })
-  
-    browser()
-    
-  } )
-}
 
-plotCoefficients(dataset = models, 
-                 combinations = c("localTransitional", "neighborhoodTransitional"), 
-                 species = c("BBWA", "CONW"))
+  })
+
+  names(coefTable) <- combinations
+
+  colToAdd <- lapply(X = combinations, FUN = function(x){
+
+    typeDisturbance <- ifelse(grepl("local", x),"LOCAL",
+                              ifelse(grepl("Local", x),"LOCAL UNDISTURBED","NEIGHBORHOOD"))
+    disturbanceDimension <- ifelse(grepl("Permanent", x),"PERMANENT",
+                                   ifelse(grepl("Transitional", x),"TRANSITIONAL","BOTH"))
+    tableCols <- data.frame(typeDisturbance, disturbanceDimension)
+
+    return(tableCols)
+
+  })
+  
+  plotTable <- Map(cbind, coefTable, colToAdd) %>%
+    do.call("rbind", .)
+  plotTable$Species <- species
+  rownames(plotTable) <- NULL
+  plotTable <- plotTable[order(plotTable$Estimate),]
+
+      plot <- ggplot(data = plotTable, aes(y = Species)) +
+                    facet_grid(typeDisturbance ~ disturbanceDimension) + #, scales = "free_y"
+                    geom_segment(aes(x = lowerCI, xend = upperCI, yend = Species)) +
+                    geom_point(aes(x = Estimate), pch=ifelse(plotTable$p < 0.05, 19, 21), 
+                                        fill=ifelse(plotTable$p < 0.05, "black", "white")) +
+                    theme(strip.text.y = element_text(size=12, face="bold"),
+                          strip.text.x = element_text(size=12, face="bold"),
+                          legend.title = element_text(face = "bold"),
+                          axis.text=element_text(size=12),
+                          axis.title=element_text(size=16,face="bold"),
+                          panel.background = element_rect(fill = "grey99"),
+                          panel.border = element_rect(colour = "black", fill=NA, size=1)) +
+                    labs(x = "Abundance estimates", y = "Bird species") +
+                  geom_vline(xintercept = 0, linetype="dashed", color="darkgrey")
+}
