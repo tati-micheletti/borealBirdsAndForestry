7f93051af377a7057189b4e0150b46809afdbc7b
tati-micheletti <tati.micheletti@gmail.com>
2018-03-13 09:12:07 -0700

    dropOutliers() :: Working perfectly

--- modules/glmerBirdModels/R/dropOutliers.R
+++ modules/glmerBirdModels/R/dropOutliers.R
@@ -1,17 +1,17 @@
 # dropOutliers
 
 dropOutliers <- function(x, dataset, name, probs){
-
-
+  
+  
   require(stats)
-
+  
   dimension <- ifelse(grepl("local", x),"State_P_100","State_P_500")
   arrayCol <- as.array(eval(parse(text = paste0("dataset[[x]]$OF_",name))))
   
-    qnt <- stats::quantile(arrayCol, probs=probs)[1]
-    rowsRemove <- which(eval(parse(text = paste0("dataset[[x]]$OF_",name)))<qnt[1] | eval(parse(text = paste0("dataset[[x]]$OF_",name)))>qnt[2])
-    newDataset <- dataset[[x]][!rowsRemove]
-    
+  qnt <- stats::quantile(arrayCol, probs=probs)[1]
+  rowsRemove <- which(eval(parse(text = paste0("dataset[[x]]$OF_",name)))<qnt[1] | eval(parse(text = paste0("dataset[[x]]$OF_",name)))>qnt[2])
+  newDataset <- dataset[[x]][!rowsRemove]
+  
   suppressMessages(assign(name, eval(parse(text = paste0("glmer(AB_", name, " ~ get(dimension) + LOG_BCR_", name, " + ", 
                                                          "offset(OF_", name,") + (1|ClusterSP) + (1|YYYY) + (1|ClusterSP:YYYY)",
                                                          ", family='poisson', data=newDataset)")))))
@@ -26,9 +26,8 @@ dropOutliers <- function(x, dataset, name, probs){
   if (!is.null(eval(parse(text = paste0(name,'@optinfo$conv$lme4$messages'))))){
     assign(name,paste(as.character("Bad, bad model. No donut for you! Convergence failed.", 
                                    "Try re-running the model with less random effects."), sep = " "))}
-  modelsList <- list()
-  modelsList[[name]] <- get(name)
-  
+
+  modelsList <- get(name)
   return(modelsList)
   
 }
