7a9594c96e1b89e29eb6aaee143c8d3d1b8928b7
Unknown <tati.micheletti@gmail.com>
2018-03-12 21:25:22 -0800

    plotCoefficients.R
    in development

--- modules/glmerBirdModels/R/plotCoefficients.R
+++ modules/glmerBirdModels/R/plotCoefficients.R
@@ -1,5 +1,10 @@
 # Plots of model coefficients (Figures 3 and 4 from Alberto's manuscript)
-
-require(lmerTest)
-
-coefTable <- as.data.table(summary(mySimOut$models$neighborhoodTransitional$CONW)$coefficients)
+# 
+# mySimOut <- readRDS(file.path(getwd(),"modelsResults_11MARCH.rds"))
+# 
+# require(lmerTest)
+# require(data.table)
+# 
+# coefTable <- as.data.table(summary(mySimOut$models$neighborhoodTransitional$BBWA)$coefficients)
+# 
+# 
