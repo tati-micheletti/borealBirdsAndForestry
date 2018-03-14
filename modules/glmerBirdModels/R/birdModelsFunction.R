01c957ab491c06e6c15694ad06127b3fba38b3bd
tati-micheletti <tati.micheletti@gmail.com>
2018-03-14 08:55:47 -0700

    Updated birdModelsFunction() :: error in models re-run the models without the ClusterSP random effect

--- modules/glmerBirdModels/R/birdModelsFunction.R
+++ modules/glmerBirdModels/R/birdModelsFunction.R
@@ -36,9 +36,26 @@ birdModelsFunction <- function(combinations, dataset, birdSp){
         return(modelsList)
 
         }, error = function(e){
-        
-          modelsList <- dropOutliers(x, dataset, name, probs = c(0.20, 0.80))
-          return(modelsList)
+          
+          suppressMessages(assign(name, eval(parse(text = paste0("glmer(AB_", name, " ~ get(dimension) + LOG_BCR_", name, " + ", 
+                                                                 "offset(OF_", name,") + (1|YYYY) + (1|ClusterSP:YYYY)",
+                                                                 ", family='poisson', data=data)")))))
+          
+          if (!is.null(eval(parse(text = paste0(name,'@optinfo$conv$lme4$messages'))))){
+            
+            suppressMessages(assign(name, eval(parse(text = paste0("glmer(AB_", name, " ~ get(dimension) + LOG_BCR_", name, " + ",
+                                                                   "offset(OF_", name,") + (1|ClusterSP) + (1|YYYY)",
+                                                                   ", family='poisson', data=data)")))))
+          }
+          
+          if (!is.null(eval(parse(text = paste0(name,'@optinfo$conv$lme4$messages'))))){
+            assign(name,paste(as.character("Bad, bad model. No donut for you! Convergence failed.", 
+                                           "Try re-running the model with less random effects."), sep = " "))
+         
+          }
+            
+            modelsList <- get(name)
+            return(modelsList)
         
       }
       ) # End tryCatch
