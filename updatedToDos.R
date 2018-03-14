7b9769b1d7a880aa1c5c048129f9b5b62a4d99da
tati-micheletti <tati.micheletti@gmail.com>
2018-03-14 08:54:26 -0700

    Updated ToDo's

--- updatedToDos.R
+++ updatedToDos.R
@@ -16,7 +16,7 @@
 # 2. Run the models in SpaDES with the whole dataset for all species; [OK]
 # 3. Redo the graphs / figures; [Partial]
 # 3.1 Plot 1: FIgure 2 [OK]
-# 3.2 Plot 2: Figure 3 and 4 []
+# 3.2 Plot 2: Figure 3 and 4 [OK]
 # 3.3 Plot 3: Figure S1 and S2
 # 3.5 Fix Table Appendix 1
 # 3.6 Fix Table S1
