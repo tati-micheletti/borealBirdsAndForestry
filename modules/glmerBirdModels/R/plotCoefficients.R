61c206d26846cb39c85fabe35720b5d730cbdb21
tati-micheletti <tati.micheletti@gmail.com>
2018-03-14 09:32:01 -0700

    Extracted the plotList part from plotCoefficients() now it only plots

--- modules/glmerBirdModels/R/plotCoefficients.R
+++ modules/glmerBirdModels/R/plotCoefficients.R
@@ -1,52 +1,10 @@
 
-plotCoefficients <- function(dataset = sim$models, combinations = sim$combinations, species = sim$birdSpecies){
-
-  require(lmerTest)
-  require(data.table)
+plotCoefficients <- function(plotList = sim$plotList){
+  
   require(ggplot2)
   require(ggalt)
-
-  coefTable <- lapply(X = combinations, FUN = function(x){
-
-    birdSpecies <- lapply(X = species, FUN = function(name){
-      
-      isolatedModel <- eval(parse(text = paste0("dataset[[x]]$",name)))
-      coef <- as.data.frame(summary(isolatedModel)$coefficients, keep.rownames = TRUE)
-      estimate <- coef["get(dimension)",][c(1:2, 4)]
-      colnames(estimate) <- c("Estimate", "Std.Error","p")
-      rownames(estimate) <- name
-      estimate$lowerCI <- estimate$Estimate - estimate$`Std.Error`
-      estimate$upperCI <- estimate$Estimate + estimate$`Std.Error`
-      
-      return(estimate)
-    })
-    
-    birdSpeciesUnlisted <- do.call(rbind, unname(birdSpecies))
-    return(birdSpeciesUnlisted)
-    
-  })
   
-  names(coefTable) <- combinations
-  
-  colToAdd <- lapply(X = combinations, FUN = function(x){
-    
-    typeDisturbance <- ifelse(grepl("local", x),"LOCAL",
-                              ifelse(grepl("Local", x),"LOCAL UNDISTURBED","NEIGHBORHOOD"))
-    disturbanceDimension <- ifelse(grepl("Permanent", x),"PERMANENT",
-                                   ifelse(grepl("Transitional", x),"TRANSITIONAL","BOTH"))
-    tableCols <- data.frame(typeDisturbance, disturbanceDimension)
-    
-    return(tableCols)
-    
-  })
-  
-  plotTable <- Map(cbind, coefTable, colToAdd) %>%
-    do.call("rbind", .)
-  plotTable$Species <- species
-  rownames(plotTable) <- NULL
-  plotTable <- plotTable[order(plotTable$Estimate),]
-  
-  plot <- ggplot(data = plotTable, aes(y = Species)) +
+plot <- ggplot(data = plotTable, aes(y = Species)) +
     facet_grid(typeDisturbance ~ disturbanceDimension) + #, scales = "free_y"
     geom_segment(aes(x = lowerCI, xend = upperCI, yend = Species)) +
     geom_point(aes(x = Estimate), pch=ifelse(plotTable$p < 0.05, 19, 21), 
@@ -60,6 +18,7 @@ plotCoefficients <- function(dataset = sim$models, combinations = sim$combinatio
           panel.border = element_rect(colour = "black", fill=NA, size=1)) +
     labs(x = "Abundance estimates", y = "Bird species") +
     geom_vline(xintercept = 0, linetype="dashed", color="darkgrey")
-  
-  return(plot)
+
+return(plot)
+
 }
