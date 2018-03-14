2f7fa259f1b91ef850ae8aee6a27076b1483067d
tati-micheletti <tati.micheletti@gmail.com>
2018-03-14 15:14:58 -0700

    plotAbundanceDisturbance() :: ready!

--- modules/glmerBirdModels/R/plotAbundanceDisturbance.R
+++ modules/glmerBirdModels/R/plotAbundanceDisturbance.R
@@ -6,27 +6,31 @@ plotAbundanceDisturbance <- function(plotList = sim$plotList){
   require(ggplot2)
   require(RColorBrewer)
 
-    distProb <- data.frame(distProb = seq(from = 0, to = 1, by = 0.1))
-    plotList <- plotList[rep(seq_len(nrow(plotList)), each=nrow(distProb)),]
-    expanded <- cbind(plotList, distProb)
-    expanded$coeff <- 1+(exp(expanded$Estimate*expanded$distProb)-1)
-    
-    predPlot <- ggplot(expanded, aes(x = coeff, y = distProb, col = typeDisturbance)) +
-      scale_x_continuous(limits = c(0, 1)) +
-      facet_grid(Species ~ disturbanceDimension) +
-      geom_line(size=3) +
-      theme(strip.text.y = element_text(size=12, face="bold"),
-            strip.text.x = element_text(size=12, face="bold"),
-            legend.position = "bottom",
-            legend.title = element_text(face = "bold"),
-            legend.title.align = 0.5,
-            axis.text=element_text(size=12),
-            axis.title=element_text(size=16,face="bold"),
-            legend.text=element_text(size=12),
-            panel.background = element_rect(fill = "grey99"),
-            panel.border = element_rect(colour = "black", fill=NA, size=1)) +
-      labs(x = "Proportion of disturbed area", y = "Density relative to intact areas")
-    
-    return(predPlot)
-    
+  distProb <- data.frame(distProb = seq(from = 0, to = 1, by = 0.1))
+  plotList <- plotList[rep(seq_len(nrow(plotList)), each=nrow(distProb)),]
+  expanded <- cbind(plotList, distProb)
+  expanded$coeff <- exp(expanded$Estimate*expanded$distProb)
+  
+  predPlot <- ggplot(expanded, aes(x = distProb, y = coeff, col = disturbanceDimension)) +
+    scale_colour_grey(start = 0.2, end = 0.8, name = "Type of Disturbance") +
+    scale_x_continuous(limits = c(0, 1), breaks = c(0, 0.5, 1), labels = c(0, 50, 100)) +
+    scale_y_continuous(limits = c(0, 2)) +
+    facet_grid(typeDisturbance ~ Species) +
+    geom_line(size=1.5) +
+    theme(strip.text.y = element_text(size=12, face="bold"),
+          strip.text.x = element_text(size=12, face="bold"),
+          legend.key = element_blank(),
+          legend.position = "bottom",
+          legend.title = element_text(face = "bold"),
+          legend.title.align = 0.5,
+          axis.text=element_text(size=10),
+          axis.title=element_text(size=16,face="bold"),
+          legend.text=element_text(size=12),
+          panel.background = element_rect(fill = "grey99"),
+          panel.border = element_rect(colour = "black", fill=NA, size=1)) +
+    labs(x = "Percentage of disturbed area", 
+         y = "Density relative to intact areas")
+
+  return(predPlot)
+  
 }
