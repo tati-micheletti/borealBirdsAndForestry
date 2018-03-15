36101e75bde86eada040c8b7d028aa9e43e247c3
tati-micheletti <tati.micheletti@gmail.com>
2018-03-15 13:52:04 -0700

    global.R :: ready.

--- global.R
+++ global.R
@@ -1,4 +1,4 @@
-# Global script for birdForestry project
+# Global script for the glmerBirdModels
 
 library(SpaDES)
 
@@ -15,43 +15,29 @@ paths <- list(
 setPaths(modulePath = paths$modulePath, inputPath = paths$inputPath, outputPath = paths$outputPath, cachePath = paths$cachePath)
 
 ## list the modules to use
-modules <- list("glmerBirdModels") #birdDensityBCR_Prov_LCC
-#modules <- list("birdDensityBCR_Prov_LCC") #
+modules <- list("glmerBirdModels")
 
 ## Set simulation and module parameters
-
-times <- list(start = 1985, end = 2000, timeunit = "year")
+times <- list(start = 1985, end = 1985, timeunit = "year")
 parameters <- list(
   .globals = list(.plotInitialTime = 1),
-  glmerBirdModels = list(cropping = TRUE, cropForModel = FALSE, start = 1985, end = 1985)
-  #,birdDensityBCR_Prov_LCC = list(start = 1985, end = 1985)
+  glmerBirdModels = list(cropping = TRUE, cropForModel = FALSE)
 )
 
-objects = list(#studyAreaName = "testArea.shp",
-  birdSpecies = c("BBWA", "BLPW", "BOCH", "BRCR",
-                  "BTNW", "CAWA", "CMWA","CONW",
-                  "OVEN", "PISI", "RBNU", "SWTH",
-                  "TEWA", "WETA", "YRWA"),
-  #birdSpecies = "CONW",
-  #birdSpecies = c("BBWA", "CONW"),
-  typeDisturbance = c("Transitional", "Permanent","Both"), 
-  disturbanceDimension = c("local", "neighborhood", "LocalUndisturbed"),
-  dataName = "Final_points_BEAD_final.csv")
-
-# Problematic birds: "CONW"
-
-#objects = list(birdSpecies = c("PISI", "BLPW"))
-# 
-# dev.useRSGD(TRUE) # do not use Rstudio graphics device
-# dev() # opens external (non-RStudio) device, which is faster
-# clearPlot()
+objects = list(birdSpecies = c("BBWA", "BLPW", "BOCH", "BRCR",
+                               "BTNW", "CAWA", "CMWA","CONW",
+                               "OVEN", "PISI", "RBNU", "SWTH",
+                               "TEWA", "WETA", "YRWA"),
+               typeDisturbance = c("Transitional", "Permanent","Both"), 
+               disturbanceDimension = c("local", "neighborhood", "LocalUndisturbed"),
+               dataName = "Final_points_BEAD_final.csv")
 
 ## Simulation setup
 mySim <- simInit(times = times, params = parameters, modules = modules, paths =  paths, objects = objects)
-mySimOut <- spades(mySim, debug = TRUE) #c("warblersPointCountBC","init")
+mySimOut <- spades(mySim, debug = TRUE)
 
-# To save
-#saveRDS(mySimOut, file.path(outputPath(mySimOut), "AlbertosModels14Mar18.rds"))
+# To save the outputs
+saveRDS(mySimOut, file.path(outputPath(mySimOut), "Results.rds"))
 
-moduleDiagram(mySim)
+# To see the objects that are interacting in this module:
 objectDiagram(mySim)
