a9d657c2f589c32ac7077bf77cc5dc6eaaf0aa3e
tati-micheletti <tati.micheletti@gmail.com>
2018-03-19 15:44:51 -0700

    Change to function loadCroppedData() :: Forgot the quotes there

--- modules/glmerBirdModels/R/loadCroppedData.R
+++ modules/glmerBirdModels/R/loadCroppedData.R
@@ -2,17 +2,17 @@
 
 #CROPS (IF NECESSARY) AND LOAD DATA
 
-loadCroppedData <- function(data){
+loadCroppedData <- function(sim, dataName){
   
   require(data.table)
   require(raster)
   require(sf)
-  
+  browser()
   if (params(sim)$glmerBirdModels$cropForModel==TRUE){
     
     # data.path <- file.path(getwd(), "modules/glmerBirdModels/data", data)
     
-    if (is.null("birdData")){ # Still to test for automatic download of data. If it doesn't work: try exists(data.path) and uncommenting line above
+    if (is.null(sim$birdData)){ # Still to test for automatic download of data. If it doesn't work: try exists(data.path) and uncommenting line above
       invisible(readline(prompt="Make dure you have the dataset in Google Drives folder 'BAM', and press [enter] to continue"))
       require(googledrive)
       drive_download(file.path("BAM",data), path = file.path(getwd(), "modules/glmerBirdModels/data", data), overwrite = FALSE,verbose = FALSE)
