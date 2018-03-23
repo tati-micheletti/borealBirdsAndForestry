81cea6f02e19a09f2d4f975c3516482197f530db
tati-micheletti <tati.micheletti@gmail.com>
2018-03-23 15:27:33 -0700

    fixed plotCoefficients() :: it was not working the significancy in the right way

--- modules/glmerBirdModels/R/plotCoefficients.R
+++ modules/glmerBirdModels/R/plotCoefficients.R
@@ -4,16 +4,17 @@ plotCoefficients <- function(sim = sim, plotList = sim$plotList){
   require(ggplot2)
   require(ggalt)
   
-  part <- plotList[plotList$typeDisturbance=="LOCAL"&plotList$disturbanceDimension=="TRANSITIONAL",]
+  part <- plotList[plotList$disturbanceDimension=="LOCAL"&plotList$typeDisturbance=="TRANSITIONAL",]
   extc <- part$Species
   plotList$newSpecies = factor(plotList$Species, levels=extc, labels=extc)
 
   plot <- ggplot(plotList, mapping = aes(x = Estimate, y = newSpecies)) +
-    facet_grid(typeDisturbance ~ disturbanceDimension, scales = "free_y") +
+    facet_grid(disturbanceDimension ~ typeDisturbance, scales = "free_y") +
     geom_segment(aes(x = lowerCI, xend = upperCI, yend = Species)) +
-    geom_point(aes(x = Estimate), pch=ifelse(plotList$p < 0.05, 19, 21), 
-               fill=ifelse(plotList$p < 0.05, "black", "white")) +
-    theme(strip.text.y = element_text(size=12, face="bold"),
+    geom_point(data = plotList, aes(x = Estimate, fill = as.factor(Significancy)), pch=21) +
+    scale_fill_manual(values = c("YES" = "black", "NO" = "white")) +
+    theme(legend.position = "none",
+          strip.text.y = element_text(size=12, face="bold"),
           strip.text.x = element_text(size=12, face="bold"),
           legend.title = element_text(face = "bold"),
           axis.text=element_text(size=12),
