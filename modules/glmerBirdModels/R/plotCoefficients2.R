8b33c95bc6ecb9812bafeb37f671a9b5617b8453
tati-micheletti <tati.micheletti@gmail.com>
2018-03-23 15:49:02 -0700

    Fixed both plotAbundanceDisturbance2() and plotCoefficients2()

--- modules/glmerBirdModels/R/plotCoefficients2.R
+++ modules/glmerBirdModels/R/plotCoefficients2.R
@@ -25,8 +25,8 @@ plotCoefficients2 <- function(sim = sim, plotList = sim$plotList){
   
   plot <- ggplot(data = plotList, aes(y = Estimate, 
                                       x = reorder_within(Species, by = Estimate, within = paste0(typeDisturbance, disturbanceDimension)))) +
-    geom_point(pch=ifelse(plotList$p < 0.05, 19, 21), 
-               fill=ifelse(plotList$p < 0.05, "black", "white")) +
+    geom_point(data = plotList, aes(x = Estimate, fill = as.factor(Significancy)), pch=21) +
+    scale_fill_manual(values = c("YES" = "black", "NO" = "white")) +
     geom_linerange(aes(ymin = lowerCI, ymax = upperCI)) +
     geom_hline(yintercept = 0, linetype="dashed", color="darkgrey") +
     scale_x_reordered() +
@@ -40,7 +40,7 @@ plotCoefficients2 <- function(sim = sim, plotList = sim$plotList){
           panel.border = element_rect(colour = "black", fill=NA, size=1)) +
     labs(y = "Abundance estimates", x = "Bird species") +
     coord_flip() +
-    facet_grid(typeDisturbance ~ disturbanceDimension, scales = "free_y") #, scales = "free_y"
+    facet_grid(disturbanceDimension ~ typeDisturbance, scales = "free_y") #, scales = "free_y"
   
   png(file.path(sim@paths$outputPath,"plotCoefficients2.png"), width = 1500, height = 863)
   plot
