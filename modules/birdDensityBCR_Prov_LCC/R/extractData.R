d0e3ac97580693ebe9a0bedde23d851b6be46327
tati-micheletti <tati.micheletti@gmail.com>
2018-06-20 19:29:09 -0700

    updated birdDensityBCR_Prov_LCC module Now it uses prepInputs

--- modules/birdDensityBCR_Prov_LCC/R/extractData.R
+++ modules/birdDensityBCR_Prov_LCC/R/extractData.R
@@ -1,34 +1,38 @@
 # Extracting the values from the rasters to feed the bird predictions
 
-extractData <- function(datasetRaster, typeData, list){
-  
-  require(data.table)
-  require(raster)
-  
-  if (typeData == "list") {
-    
-    data <- vector(mode = "list", length = length(list))
-    names(data) <- as.list(list)
-    
-    for (i in 1:length(list)){
-      
-      names(data)[i] <- list[i]
-      
-      data[[paste0(list[i])]] <- data.table(sp::coordinates(datasetRaster[[paste0(list[i])]]),
-                                            raster::values(datasetRaster[[i]]))
-      names(data[[paste0(list[i])]]) <- c("X", "Y", "Value")
-      
-    }
+# NEEDS FIXING!!!
 
-  return(data)
-  
-  } else {
-    
-      data <- data.table(coordinates(datasetRaster),
-                         raster::values(datasetRaster))
-      names(data) <- c("X", "Y", "Value")  
-      
-      return(data)
-  }
-
-}
+# extractData <- function(datasetRaster, typeData, list){
+#   
+#   require(data.table)
+#   require(raster)
+# 
+#   browser()
+#   
+#   if (typeData == "list") {
+#     
+#     data <- vector(mode = "list", length = length(list))
+#     names(data) <- as.list(list)
+#     
+#     for (i in 1:length(list)){
+#       
+#       names(data)[i] <- list[i]
+#       
+#       data[[paste0(list[i])]] <- data.table(sp::coordinates(datasetRaster[[paste0(list[i])]]),
+#                                             raster::values(datasetRaster[[i]]))
+#       names(data[[paste0(list[i])]]) <- c("X", "Y", "Value")
+#       
+#     }
+# 
+#   return(data)
+#   
+#   } else {
+#     
+#       data <- data.table(sp::coordinates(datasetRaster),
+#                          raster::values(datasetRaster))
+#       names(data) <- c("X", "Y", "Value")  
+#       
+#       return(data)
+#   }
+# 
+# }
