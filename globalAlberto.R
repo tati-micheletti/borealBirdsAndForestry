1ac747aed921ad981cfd65d5d06de6a5ecbd0148
tati-micheletti <tati.micheletti@gmail.com>
2018-06-25 22:25:08 -0700

    fixed external plots

--- globalAlberto.R
+++ globalAlberto.R
@@ -65,7 +65,7 @@ mySimOut <- spades(mySim, debug = TRUE)
 # To save the outputs
 mySimList <- as(mySimOut, "simList_")
 saveRDS(mySimList, file.path(outputPath(mySimOut), "Results25JUN18.rds"))
-source(file.path(workDirectory, "modules/glmerBirdModels/R/saveExternal.R"))
+source(file.path(workDirectory, "modules/glmerBirdModels/R/savePlotsExternal.R"))
 
 # To load the outputs
 # mySimOut <- readRDS(file.path(outputPath(mySimOut), "Results19JUN18.rds"))
