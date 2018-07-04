401233d7acd38677bfdb5906404fed96948a0829
tati-micheletti <tati.micheletti@gmail.com>
2018-07-04 13:48:34 -0700

    added comments and gdalUtils prep

--- modules/splitModelPlot/R/groupSplitRaster.R
+++ modules/splitModelPlot/R/groupSplitRaster.R
@@ -15,12 +15,14 @@ groupSplitRaster <- function(models = models,
                              disturbanceClass = disturbanceClass,
                              intermPath = intermPath) {
   
-  rasterList <- list("distType" = disturbanceType, "distYear" = disturbanceYear, "land" = landCover) 
+  rasterList <- list("distType" = disturbanceType, "distYear" = disturbanceYear, "land" = landCover) # Original rasters' list
   rasterList[["birdDensityRasters"]] <- birdDensityRasters
-  newlist <- Cache(Map, rasterList, path = file.path(pathData, names(rasterList)), f =  splitRaster,
+  newlist <- Cache(Map, rasterList, path = file.path(pathData, names(rasterList)), f =  splitRaster, # Splitted rasters' list
                    MoreArgs = list(nx = nx, ny = ny, buffer = buffer, rType = rType))
-  
+
   lengthvect <- 1:(nx * ny)
+  
+  # Outlist is a list of all the tiles after running the predictions
   outList <- lapply(lengthvect, FUN = tileReorder, 
                     inList = newlist, 
                     origList = rasterList, 
@@ -32,19 +34,32 @@ groupSplitRaster <- function(models = models,
                     passedModel = models,
                     pathData = pathData,
                     intermPath = intermPath)
-  
+
   lengthResultRasters <- 1:length(outList[[1]])
   finalRasList <- lapply(lengthResultRasters, function(nRas){
+    browser()
     rasList <- lapply(X = outList, `[[`, nRas)
     rasName <- names(outList[[1]])[nRas]
-    browser()
     dir.create(file.path(intermPath, "outputRasters"), showWarnings = FALSE)
     finalRasPath <- file.path(intermPath, "outputRasters", paste0(rasName, ".tif"))
-    tempTilesPath <- file.path("/mnt/storage/", "borealBirdsAndForestry", paste0(rasName))
-    gdalUtils::mosaic_rasters(gdalfile = unlist(rasList),
-                                dst_dataset = finalRasPath)
-      
-    return(finalRasPath)
+
+    # IF USING gdalUtils::mosaic_rasters()
+      # Define a directory for the intermediate rasters, as gdalUtils needs them from disk
+    # dir.create(file.path(intermPath, "intermediateRasters"), showWarnings = FALSE)
+    # intermPathFull <- file.path(intermPath, "intermediateRasters")
+    # tempRasNames <- lapply(X = rasList, FUN = function(x){
+    #   # Write to disk tiles to be put together
+    #   tmpFile <- tempfile(pattern = rasName, tmpdir = intermPathFull, fileext = ".tif")
+    #   writeRaster(x = x, filename = tmpFile)
+    #   return(tmpFile)
+    # })
+    
+    # gdalUtils::mosaic_rasters(gdalfile = unlist(tempRasNames),
+    #                             dst_dataset = finalRasPath)
+    
+    # rm(rasName) # Remove raster from memory
+    # gc() # Clean their memory # Check the package for arguments. Don't forget to parelell: idea: paralell the tiles' lapply.  
+    # return(finalRasPath)
       
   })
   
