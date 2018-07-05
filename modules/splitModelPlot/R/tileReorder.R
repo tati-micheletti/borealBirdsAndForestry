20afe3a0a4719337250c5ec9e9363cb106c71288
tati-micheletti <tati.micheletti@gmail.com>
2018-07-05 13:58:28 -0700

    passing only big raster's names

--- modules/splitModelPlot/R/tileReorder.R
+++ modules/splitModelPlot/R/tileReorder.R
@@ -2,7 +2,7 @@ tileReorder <- function(inList = newlist, origList = rasterList, c, pathData = p
   
   #Subset corresponding tiles 
   tilelist <- lapply(inList, '[[', c)
-  names(tilelist) <- names(origList)
+  names(tilelist) <- origList
 
   #reclassify and mask them (and run focal distances)
   processed <- LCReclassify(inputTiles = tilelist, pathData = pathData, intermPath = intermPath, ...)
