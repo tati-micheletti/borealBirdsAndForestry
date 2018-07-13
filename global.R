277934ec994f1b1a47723b072a6fc206e6501423
tati-micheletti <tati.micheletti@gmail.com>
2018-07-13 13:11:37 -0700

    Global cleaned

--- global.R
+++ global.R
@@ -1,16 +1,22 @@
-# Global script for the glmerBirdModels
+# Global script for the Backcasting Project
 
-#devtools::install(local = FALSE)
+# Adapt west are the new ones 1Km current
+
+# Bird density data: https://borealbirds.databasin.org/galleries/143b56bbc7584bd7a44ba86119061b15
+# What I need is the Breeding densities for each combination of BCR, jurisdiction, and habitat class. Internet address for this table?
+# 
+
+# STILL NEED TO CHANGE testArea = FALSE to use boreal shapefile to crop and mask to
+# CAWA: Canada Warbler
+# BBWA: Bay-breasted Warbler
+# CMWA: Cape May Warbler
+# BTNW: Black-throated Green Warbler
+
 library(SpaDES.core)
 library(SpaDES.tools)
 
-#debug(fixErrors)
-
 # set the directories
  workDirectory <- getwd()
-
-#workDirectory <- "/home/tmichele/Documents/GitHub/borealBirdsAndForestry" #For working on prepInputs
-#debug(cropInputs)
 
 paths <- list(
   # As the project takes up a LOT of space, all mid steps will be saved inside the cache folder of another partition,
@@ -28,59 +34,63 @@ setPaths(modulePath = paths$modulePath, inputPath = paths$inputPath, outputPath 
 # }
 
 ## list the modules to use
-modules <- list("birdDensityBCR_Prov_LCC", "glmerBirdModels", "splitModelPlot") # #bayesianBirdModel
+modules <- list("glmerBirdModels") # "birdDensityBCR_Prov_LCC", #bayesianBirdModel, "splitModelPlot"
 
 ## Set simulation and module parameters
-times <- list(start = 1997, end = 1998, timeunit = "year") # [ FIX ] Make an if statement the first time you set time: if any(times > 1000) times - 1900 to make it flexible to both YY or YYYY.
+times <- list(start = 1985, end = 2011, timeunit = "year")
 parameters <- list(
-    bayesianBirdModel = list(testArea = TRUE),
+    birdDensityBCR_Prov_LCC = list(extractFrom4kRasters = TRUE),
+    bayesianBirdModel = list(testArea = TRUE), # FALSE means using boral shapefile to crop and mask
     glmerBirdModels = list(cropForModel = FALSE),
-    splitModelPlot = list(testArea = TRUE,
+    splitModelPlot = list(recoverTime = 30,
+                          testArea = TRUE,
                           focalDistance = 100, # To run for neighborhood, change to 500
                           disturbanceClass = 2, # 2 = Forestry, 1 = Fire, 3 and 4 = low probability forestry and fire
-                          nx = 1, # mult 7
-                          ny = 10, # mult 3
+                          nx = 3, # mult 7
+                          ny = 4, # mult 3
                           rType = "INT1U",
                           buffer = c(18,18),
                           forestClass = 1:6,
                           .useCache = FALSE,
-                          useParallel = NULL) # "across" = across machines, "local" = only on local machine, "NULL" or anything else = no parallel
+                          useParallel = "across") # "across" = across machines, "local" = only on local machine, "NULL" or anything else = no parallel
+    # As of 12th July, 'across' is not working... See notes above
 )
 
 objects <- list(
-    dataName = "Final_points_2010.csv",
-    birdSpecies = c("BBWA" #,
-                     # "BLPW", 
-                    # "BOCH", "BRCR",
-                     # "BTNW", "CAWA", 
-                    # "CMWA","CONW",
-                    # "OVEN", "PISI",
-                    # "RBNU", "SWTH",
-                    # "TEWA", "WETA", 
-                     # "YRWA"
+    dataName = "Final_points_2010.csv", #Manuscript file was Final_points_2010.csv; testing to compare with Final_points_2010_updatedDensity.csv
+    birdSpecies = c("BBWA",
+                    "BLPW", 
+                    "BOCH", 
+                    "BRCR",
+                    "BTNW", 
+                    "CAWA", 
+                    "CMWA",
+                    "CONW",
+                    "OVEN", 
+                    "PISI",
+                    "RBNU", 
+                    "SWTH",
+                    "TEWA",
+                    "WETA", 
+                    "YRWA"
                     ),
-    typeDisturbance = c("Transitional", "Permanent"), #, "Permanent", "Both"
-    disturbanceDimension = c("local", "neighborhood"), #, "neighborhood", "LocalUndisturbed"
+    typeDisturbance = c("Transitional", "Permanent", "Both"), #, "Permanent", "Both"
+    disturbanceDimension = c("local", "neighborhood", "LocalUndisturbed"), #, "neighborhood", "LocalUndisturbed"
     disturbancePredict = c("Transitional") # Needs to match disturbanceClass from prediction module
 )
 
-## Using external viewer
-# dev.useRSGD(FALSE) # do not use sRstudio graphics device
-# dev() # opens external (non-RStudio) device, which is faster
+clearPlot()
 
-# clearPlot()
+# file.remove("/mnt/storage/borealBirdsAndForestry/cache/logParallel")
 
-# debug(postProcess)
 ## Simulation setup
 mySim <- simInit(times = times, params = parameters, modules = modules, paths =  paths, objects = objects)
 mySimOut <- spades(mySim, debug = TRUE)
 
 # To save the outputs
 # mySimList <- as(mySimOut, "simList_")
-# saveRDS(mySimList, file.path(outputPath(mySimOut), "bayesResults.rds"))
+# saveRDS(mySimList, file.path(outputPath(mySimOut), "backcast_10July2018.rds"))
 
 # To load the outputs
-# mySimOut <- readRDS(file.path(outputPath(mySimOut), "Results_List.rds"))
+# mySimOut <- readRDS(file.path(outputPath(mySimOut), "backcast_10July2018.rds"))
 
-# To see the objects that are interacting in this module:
-# objectDiagram(mySim)
