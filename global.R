7d308b0d968a87eaa566241027764bada9f24709
tati-micheletti <tati.micheletti@gmail.com>
2018-07-25 11:20:48 -0700

    global.R file with SQL modifications

--- global.R
+++ global.R
@@ -1,10 +1,6 @@
 # Global script for the Backcasting Project
 
-# Adapt west are the new ones 1Km current
-
 # Bird density data: https://borealbirds.databasin.org/galleries/143b56bbc7584bd7a44ba86119061b15
-# What I need is the Breeding densities for each combination of BCR, jurisdiction, and habitat class. Internet address for this table?
-# 
 
 # STILL NEED TO CHANGE testArea = FALSE to use boreal shapefile to crop and mask to
 # CAWA: Canada Warbler
@@ -12,11 +8,23 @@
 # CMWA: Cape May Warbler
 # BTNW: Black-throated Green Warbler
 
+# IF NEED TO WORK ON PREPINPUTS
+# try(detach("package:SpaDES.addins", unload = TRUE)); 
+# try(detach("package:SpaDES.core", unload = TRUE)); 
+# try(detach("package:SpaDES.tools", unload = TRUE)); 
+# try(detach("package:reproducible", unload = TRUE)); 
+# devtools::load_all("~/Documents/GitHub/reproducible"); 
+# # library(SpaDES.core)
+## devtools::load_all("~/Documents/GitHub/SpaDES.core")
+# library(SpaDES.tools)
+##devtools::load_all("~/Documents/GitHub/SpaDES.tools"); 
+
 library(SpaDES.core)
 library(SpaDES.tools)
 
 # set the directories
- workDirectory <- getwd()
+ # workDirectory <- getwd()
+workDirectory <- "/home/tmichele/Documents/GitHub/borealBirdsAndForestry"
 
 paths <- list(
   # As the project takes up a LOT of space, all mid steps will be saved inside the cache folder of another partition,
@@ -34,12 +42,13 @@ setPaths(modulePath = paths$modulePath, inputPath = paths$inputPath, outputPath 
 # }
 
 ## list the modules to use
-modules <- list("glmerBirdModels") # "birdDensityBCR_Prov_LCC", #bayesianBirdModel, "splitModelPlot"
+modules <- list("loadOffsetsBAM", "birdDensityBCR_Prov_LCC", "glmerBirdModels") #  #bayesianBirdModel, #,  "splitModelPlot"
 
 ## Set simulation and module parameters
 times <- list(start = 1985, end = 2011, timeunit = "year")
 parameters <- list(
-    birdDensityBCR_Prov_LCC = list(extractFrom4kRasters = TRUE),
+    birdDensityBCR_Prov_LCC = list(extractFrom4kRasters = FALSE,
+                                   testArea = TRUE),
     bayesianBirdModel = list(testArea = TRUE), # FALSE means using boral shapefile to crop and mask
     glmerBirdModels = list(cropForModel = FALSE),
     splitModelPlot = list(recoverTime = 30,
@@ -53,29 +62,36 @@ parameters <- list(
                           forestClass = 1:6,
                           .useCache = FALSE,
                           useParallel = "across") # "across" = across machines, "local" = only on local machine, "NULL" or anything else = no parallel
-    # As of 12th July, 'across' is not working... See notes above
+    # As of 12th July, 'across' is not working... See notes on ToDo.R
 )
 
 objects <- list(
-    dataName = "Final_points_2010.csv", #Manuscript file was Final_points_2010.csv; testing to compare with Final_points_2010_updatedDensity.csv
+    SQLtableVersion = "V4_2015",
+    SQLServer = "boreal.biology.ualberta.ca",
+    SQLDatabase = "BAM_National_V4_2015_0206",
+    densityEstimatesURL = "https://drive.google.com/open?id=1Gd9VsKIXX_384UxQsJnwzsSZFuyQyscw",
+    densityEstimatesFileName = "Habitat_Association_by_jurisdiction(bamddb_Apr19-2012).csv",
+    newestDataset = "offsets-v3-new-chunk_2017-05-26.Rdata",
+    olderDataset = "offsets-v3_2017-04-19.Rdata", # If the file only has one dataset, especify it as newestDataset and NULL olderDataset
+    dataName = "Final_points_2010.csv", #Manuscript file was Final_points_2010.csv; test to compare with Final_points_2010_updatedDensity.csv
     birdSpecies = c("BBWA",
-                    "BLPW", 
-                    "BOCH", 
-                    "BRCR",
-                    "BTNW", 
-                    "CAWA", 
-                    "CMWA",
-                    "CONW",
-                    "OVEN", 
-                    "PISI",
-                    "RBNU", 
-                    "SWTH",
-                    "TEWA",
-                    "WETA", 
+                    # "BLPW", 
+                    # "BOCH", 
+                    # "BRCR",
+                    # "BTNW", 
+                    # "CAWA", 
+                    # "CMWA",
+                    # "CONW",
+                    # "OVEN", 
+                    # "PISI",
+                    # "RBNU", 
+                    # "SWTH",
+                    # "TEWA",
+                    # "WETA", 
                     "YRWA"
                     ),
-    typeDisturbance = c("Transitional", "Permanent", "Both"), #, "Permanent", "Both"
-    disturbanceDimension = c("local", "neighborhood", "LocalUndisturbed"), #, "neighborhood", "LocalUndisturbed"
+    typeDisturbance = c("Transitional", "Permanent"), #, "Permanent", "Both"
+    disturbanceDimension = c("local", "neighborhood"), #, "neighborhood", "LocalUndisturbed"
     disturbancePredict = c("Transitional") # Needs to match disturbanceClass from prediction module
 )
 
