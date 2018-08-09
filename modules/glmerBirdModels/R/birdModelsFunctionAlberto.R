2f5dd0869a203fb3064f26a674244aaccdbcd198
tati-micheletti <tati.micheletti@gmail.com>
2018-08-09 16:52:50 -0700

    birdModelsFunctionAlberto() Models to run with Alberto's version of the dataset

---
+++ modules/glmerBirdModels/R/birdModelsFunctionAlberto.R
@@ -0,0 +1,121 @@
+# Bird model functions version 2 to use with Alberto's data exclusively - TO BE USED WHEN PREDICTING
+
+birdModelsFunctionAlberto <- function(combinations = sim$combinations,
+                               birdSp = sim$birdSpecies,
+                               dataset = "data",
+                               simEnv = envir(sim)){
+  
+  require(lme4)
+  
+  dataset <- get(dataset, envir = simEnv)
+  
+  models <- list()
+  modelsList <- list()
+  errorModels <- list()
+  
+  models <- lapply(X = combinations, FUN = function(x){
+    if (grepl("local", x)) {
+      data <- dataset[[x]]
+      birds <- lapply(X = birdSp, FUN = function(name){
+        
+        tryCatch({
+          
+          suppressMessages(assign(name, eval(parse(text = paste0("glmer(AB_", name, " ~ State_P_100 + LOG_BCR_", name, " + ",
+                                                                 "(1|ClusterSP) + (1|YYYY) + (1|ClusterSP:YYYY), ",
+                                                                 "offset = OF_", name,
+                                                                 ", family = 'poisson', data = data)")))))
+          
+          if (!is.null(eval(parse(text = paste0(name,'@optinfo$conv$lme4$messages'))))){
+            
+            suppressMessages(assign(name, eval(parse(text = paste0("glmer(AB_", name, " ~ State_P_100 + LOG_BCR_", name, " + ",
+                                                                   "(1|ClusterSP) + (1|YYYY), ",
+                                                                   "offset = OF_", name,
+                                                                   ", family = 'poisson', data = data)")))))
+          }
+          
+          if (!is.null(eval(parse(text = paste0(name,'@optinfo$conv$lme4$messages'))))){
+            
+            suppressMessages(assign(name, eval(parse(text = paste0("glmer(AB_", name, " ~ State_P_100 + LOG_BCR_", name, " + ",
+                                                                   "(1|ClusterSP), ",
+                                                                   "offset = OF_", name,
+                                                                   ", family = 'poisson', data = data)")))))
+          }
+          
+          if (!is.null(eval(parse(text = paste0(name,'@optinfo$conv$lme4$messages'))))){
+            
+            suppressMessages(assign(name, eval(parse(text = paste0("glme(AB_", name, " ~ State_P_100 + LOG_BCR_", name, " + ",
+                                                                   "offset = OF_", name,
+                                                                   ", family = 'poisson', data = data)")))))
+          }
+          
+          modelsList <- get(name)
+          return(modelsList)
+          
+        }, error = function(e){
+          
+          assign(name,paste(as.character("Bad, bad model. No donut for you! Convergence failed.", 
+                                         "Try re-running the model with less random effects."), sep = " "))
+          modelsList <- get(name)
+          return(modelsList)
+        }
+        ) # End tryCatch
+        
+      })
+    } else {
+      data <- dataset[[x]]
+      birds <- lapply(X = birdSp, FUN = function(name){
+        
+        tryCatch({
+          
+          suppressMessages(assign(name, eval(parse(text = paste0("glmer(AB_", name, " ~ State_P_500 + LOG_BCR_", name, " + ", 
+                                                                 "(1|ClusterSP) + (1|YYYY) + (1|ClusterSP:YYYY), ",
+                                                                 "offset = OF_", name,
+                                                                 ", family='poisson', data=data)")))))
+          
+          if (!is.null(eval(parse(text = paste0(name,'@optinfo$conv$lme4$messages'))))){
+            
+            suppressMessages(assign(name, eval(parse(text = paste0("glmer(AB_", name, " ~ State_P_500 + LOG_BCR_", name, " + ",
+                                                                   "(1|ClusterSP) + (1|YYYY), ",
+                                                                   "offset = OF_", name,
+                                                                   ", family='poisson', data=data)")))))
+          }
+          
+          if (!is.null(eval(parse(text = paste0(name,'@optinfo$conv$lme4$messages'))))){
+            
+            suppressMessages(assign(name, eval(parse(text = paste0("glmer(AB_", name, " ~ State_P_500 + LOG_BCR_", name, " + ",
+                                                                   "(1|ClusterSP), ",
+                                                                   "offset = OF_", name,
+                                                                   ", family = 'poisson', data = data)")))))
+          }
+          
+          if (!is.null(eval(parse(text = paste0(name,'@optinfo$conv$lme4$messages'))))){
+            
+            suppressMessages(assign(name, eval(parse(text = paste0("glme(AB_", name, " ~ State_P_500 + LOG_BCR_", name, " + ",
+                                                                   "offset = OF_", name,
+                                                                   ", family = 'poisson', data = data)")))))
+          }
+          
+          modelsList <- get(name)
+          return(modelsList)
+          
+        }, error = function(e){
+          
+          assign(name,paste(as.character("Bad, bad model. No donut for you! Convergence failed.", 
+                                         "Try re-running the model with less random effects."), sep = " "))
+          
+          modelsList <- get(name)
+          return(modelsList)
+        }
+        ) # End tryCatch
+        
+      })
+    }
+    
+    names(birds) <- birdSp
+    return(birds)
+  })
+  
+  names(models) <- combinations
+  return(models)
+  
+}
