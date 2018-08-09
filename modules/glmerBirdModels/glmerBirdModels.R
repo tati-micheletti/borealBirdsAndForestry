49e460151f0b2890ec35d05061cb59f28e87c9ff
tati-micheletti <tati.micheletti@gmail.com>
2018-08-09 16:44:41 -0700

    updated the glmerBirdModel Now it is easier to see the expected inputs and outputs!

--- modules/glmerBirdModels/glmerBirdModels.R
+++ modules/glmerBirdModels/glmerBirdModels.R
@@ -1,11 +1,9 @@
-
-
 defineModule(sim, list(
   name = "glmerBirdModels",
-  description = c("The present module is based on the work of Suárez-Esteban et al. (in prep). These models investigate the following questions: 1. Do cumulative disturbances at local scales have a consistent effect across Canada on old forest-associated songbirds? 2. Do neighborhood disturbances influence the abundance of birds at local scales? 3. What are the relative effects of permanent and transitional disturbance agents at both scales?. For more information: Alberto Suárez-Esteban, Steve G. Cumming, Erin M. Bayne, Samantha J. Song, and Fiona K. A. Schmiegelow. The rise of industrial development, the fall of boreal songbirds: Industrial development threatens boreal songbirds across Canada"),
+  description = c("The present module is based on the work of SuÃ¡rez-Esteban et al. (in prep). These models investigate the following questions: 1. Do cumulative disturbances at local scales have a consistent effect across Canada on old forest-associated songbirds? 2. Do neighborhood disturbances influence the abundance of birds at local scales? 3. What are the relative effects of permanent and transitional disturbance agents at both scales?. For more information: Alberto SuÃ¡rez-Esteban, Steve G. Cumming, Erin M. Bayne, Samantha J. Song, and Fiona K. A. Schmiegelow. The rise of industrial development, the fall of boreal songbirds: Industrial development threatens boreal songbirds across Canada"),
   keywords = c("songbirds", "forestry", "Anthropogenic disturbance"),
   authors = c(person("Tati", "Micheletti", email = "tati.micheletti@gmail.com", role = c("aut", "cre")),
-              person("Alberto", "Suárez-Esteban", email = "suarezes@ualberta.ca", role = c("aut"))),
+              person("Alberto", "Suarez-Esteban", email = "suarezes@ualberta.ca", role = c("aut"))),
   childModules = character(0),
   version = list(SpaDES.core = "0.1.0.9000", glmerBirdModels = "0.0.1"),
   spatialExtent = raster::extent(rep(NA_real_, 4)),
@@ -13,46 +11,38 @@ defineModule(sim, list(
   timeunit = "year",
   citation = list("citation.bib"),
   documentation = list("README.txt", "glmerBirdModels.Rmd"),
-  reqdPkgs = list("data.table", "rgdal", "raster", "sf", "lme4", "googledrive", "DBI", "odbc"),
+  reqdPkgs = list("data.table", "rgdal", "raster", "sf", "lme4", "googledrive"),
   parameters = rbind(
-    #defineParameter("paramName", "paramClass", value, min, max, "parameter description"),
     defineParameter("cropForModel", "logical", FALSE, NA, NA, "If the bird data should be cropped to a study area or not for fitting the model"),
+    defineParameter("avoidAlbertosData", "logical", TRUE, NA, NA, "Should we use the most updated version of the BAM densities?"),
     defineParameter(".plotInitialTime", "numeric", 1, NA, NA, "This describes the simulation time at which the first plot event should occur"),
     defineParameter(".plotInterval", "numeric", NA, NA, NA, "This describes the simulation time interval between plot events"),
     defineParameter(".saveInitialTime", "numeric", NA, NA, NA, "This describes the simulation time at which the first save event should occur"),
     defineParameter(".saveInterval", "numeric", NA, NA, NA, "This describes the simulation time interval between save events"),
     defineParameter(".useCache", "logical", FALSE, NA, NA, "Should this entire module be run with caching activated? This is generally intended for data-type modules, where stochasticity and time are not relevant")
-  ), # Caching events
+  ),
   inputObjects = bind_rows(
-    #expectsInput("objectName", "objectClass", "input object description", sourceURL, ...),
-    expectsInput(objectName = c("birdData", "studyArea", "typeDisturbance", "disturbanceDimension", 
-                                "birdSpecies", "dataName", "studyAreaName", "SQLtableVersion", "SQLServer", "SQLDatabase"), 
-                 objectClass = c("data.table","character", "character", "character", "character", "character", "character",
-                                 "character", "character", "character"), 
-                 desc = c("Bird data assembled by the BAM (Boreal Avian Modelling Project)",
-                          "Character to define the are to crop",
-                          "Might be Transitional, Permanent, Undisturbed, and/or Both",
-                          "Might be local and/or neighborhood",
-                          "List of bird species to be modeled",
-                          "File name and extension of original data file",
-                          "Name of the file and extension to crop for study area",
-                          "Table version of the BAM data in the SQL for which to filter tables", "path for SQL sever", "Name of SQL database"),
-                 sourceURL = c(NA,NA,NA,NA,NA,NA, NA, NA, NA, NA))
+    expectsInput(objectName = "birdData", objectClass = "data.table", desc = "Bird data assembled by the BAM (Boreal Avian Modelling Project)", sourceURL = NA),
+    expectsInput(objectName = "studyArea", objectClass = "character", desc = "Character to define the area to crop", sourceURL = NA),
+    expectsInput(objectName = "typeDisturbance", objectClass = "character", desc = "Might be Transitional, Permanent, Undisturbed, and/or Both", sourceURL = NA),
+    expectsInput(objectName = "disturbanceDimension", objectClass = "character", desc = "Might be local and/or neighborhood", sourceURL = NA),
+    expectsInput(objectName = "birdSpecies", objectClass = "character", desc = "List of bird species to be modeled", sourceURL = NA),
+    expectsInput(objectName = "dataName", objectClass = "character", desc = "File name and extension of original data file", sourceURL = NA),
+    expectsInput(objectName = "studyAreaName", objectClass = "character", desc = "Name of the file and extension to crop for study area", sourceURL = NA),
+    expectsInput(objectName = "birdDensityDS", objectClass = "data.table", desc = "list of data.table with information on species densities based on LCC and BCR/Prov", sourceURL = NA),
+    expectsInput(objectName = "offsetsBySpecies", objectClass = "data.table", desc = "Offset values to correct for bird counts when using BAM dataset", sourceURL = NA)
   ),
   outputObjects = bind_rows(
-    #createsOutput("objectName", "objectClass", "output object description", ...),
-    createsOutput(objectName = c("models","data","plotDistSec", "plotCoeff", "plotList", "plotAbundDist", "tableSampling", "AIC", "combinations"), #"studyArea"
-                  objectClass = c("list","list","plot","plot", "data.table", "plot", "data.table", "data.table", "character"), #,"shapefile"
-                  desc = c("list of boreal bird models", #"shapefile of the study area",
-                           "list of the data already subsetted for the models",
-                           "Plot of disturbance sectors",
-                           "Plot of coefficients of bird models",
-                           paste0("Data table of all models estimates, and std.error",
-                                  " of % of disturbance per species, type and dimension of disturbance"),
-                           "Plot of relative density per disturbance type, dimension and species on varying disturbance proportions",
-                           "Table with the number of samples for each type and dimension",
-                           "Table with all AIC values for all models",
-                           "total combination of type and dimension of disturbances"))
+    createsOutput(objectName = "models", objectClass = "list", desc = "list of boreal bird models"),
+    createsOutput(objectName = "data", objectClass = "list", desc = "list of the data already subsetted for the models"),
+    createsOutput(objectName = "plotDistSec", objectClass = "plot", desc = "Plot of disturbance sectors"),
+    createsOutput(objectName = "plotCoeff", objectClass = "plot", desc = "Plot of coefficients of bird models"),
+    createsOutput(objectName = "plotList", objectClass = "data.table", desc = paste0("Data table of all models estimates, and std.error",
+                                                                                     " of % of disturbance per species, type and dimension of disturbance")),
+    createsOutput(objectName = "plotAbundDist", objectClass = "plot", desc = "Plot of relative abundance per disturbance type, dimension and species on varying disturbance proportions"),
+    createsOutput(objectName = "tableSampling", objectClass = "data.table", desc = "Table with the number of samples for each type and dimension"),
+    createsOutput(objectName = "AIC", objectClass = "data.table", desc = "Table with all AIC values for all models"),
+    createsOutput(objectName = "combinations", objectClass = "character", desc = "total combination of type and dimension of disturbances")
   )
 ))
 
@@ -66,19 +56,10 @@ doEvent.glmerBirdModels = function(sim, eventTime, eventType, debug = FALSE) {
       sim <- Init(sim)
       
       # schedule future event(s)
-      sim <- scheduleEvent(sim, start(sim), "glmerBirdModels", "retrievingSQLData")
       sim <- scheduleEvent(sim, start(sim), "glmerBirdModels", "dataUploading")
       sim <- scheduleEvent(sim, start(sim), "glmerBirdModels", "birdModels")
       sim <- scheduleEvent(sim, start(sim), "glmerBirdModels", "plots")
       sim <- scheduleEvent(sim, start(sim), "glmerBirdModels", "save")
-    },
-    
-    retrievingSQLData = {
-      
-      sim$SQLData <- Cache(retrieveSQLData, SQLtableVersion = sim$SQLtableVersion,
-                           SQLServer = sim$SQLServer,
-                           SQLDatabase = sim$SQLDatabase,
-                           birdSpecies = sim$birdSpecies)
     },
     
     dataUploading = {
@@ -86,20 +67,33 @@ doEvent.glmerBirdModels = function(sim, eventTime, eventType, debug = FALSE) {
       if (params(sim)$glmerBirdModels$cropForModel == TRUE) {
         sim$data <- sim$birdData
       } else {
+        
+        sim$SQLData <- retrieveSQLData(SQLtableVersion = sim$SQLtableVersion,
+                                       SQLServer = sim$SQLServer,
+                                       SQLDatabase = sim$SQLDatabase,
+                                       birdSpecies = sim$birdSpecies)
 
         sim$data <- dataUploading(data = sim$dataName, 
                                   combinations =  sim$combinations,
-                                  birdDensityRasters = sim$birdDensityRasters,
+                                  birdDensityDS = sim$birdDensityDS,
+                                  offsetDT = sim$offsetsBySpecies,
+                                  avoidAlbertosData = P(sim)$avoidAlbertosData,
                                   SQLTableName = "SQLData",
                                   envirSim = envir(sim))
-      }
+        }
     },
     birdModels = {
       
-      sim$models <- Cache(birdModelsFunction,
-                          combinations = sim$combinations,
-                          dataset = sim$dataName,
-                          birdSp = sim$birdSpecies)
+      if (P(sim)$avoidAlbertosData == FALSE) {
+      sim$models <- Cache(birdModelsFunctionAlberto, combinations = sim$combinations,
+                                       dataset = "data",
+                                       birdSp = sim$birdSpecies,
+                                       simEnv = envir(sim))
+    } else {
+      sim$models <- Cache(birdModelsFunction, combinations = sim$combinations,
+                          birdSp = sim$birdSpecies,
+                          dataset = sim$data)
+    }
       
     },
     plots = {
@@ -107,17 +101,17 @@ doEvent.glmerBirdModels = function(sim, eventTime, eventType, debug = FALSE) {
       sim$plotDistSec <- plotDisturbanceSector(dataset = sim$data,
                                                types = sim$typeDisturbance,
                                                outputPath = outputPath(sim))
-
-       sim$plotList <- plotList(dataset = sim$models,
-                                combinations = sim$combinations,
-                                birdSp = sim$birdSpecies,
-                                outputPath = outputPath(sim))
       
-       sim$plotCoeff <- plotCoefficients(plotList = sim$plotList,
-                                         outputPath = outputPath(sim))
+      sim$plotList <- plotList(dataset = sim$models,
+                               combinations = sim$combinations,
+                               birdSp = sim$birdSpecies,
+                               outputPath = outputPath(sim))
       
-       sim$plotAbundDist <- plotAbundanceDisturbance(plotList = sim$plotList,
-                                                     outputPath = outputPath(sim))
+      sim$plotCoeff <- plotCoefficients(plotList = sim$plotList,
+                                        outputPath = outputPath(sim))
+      
+      sim$plotAbundDist <- plotAbundanceDisturbance(plotList = sim$plotList,
+                                                    outputPath = outputPath(sim))
       
     },
     save = {
@@ -125,7 +119,7 @@ doEvent.glmerBirdModels = function(sim, eventTime, eventType, debug = FALSE) {
       sim$tableSampling <- tableSampling(dataName = sim$dataName,
                                          dataset = sim$data,
                                          outputPath = outputPath(sim))
-
+      
       sim$AIC <- tableAIC(models = sim$models,
                           birdSp = sim$birdSpecies,
                           combinations = sim$combinations,
@@ -156,30 +150,19 @@ Init <- function(sim) {
     sim$studyArea <- loadStudyArea(data = "testArea.shp")
     sim$birdData <- loadCroppedData(sim = sim, dataName = "Final_points_2010.csv")
   }
-   if (!suppliedElsewhere("birdSpecies", sim)){
+  if (!suppliedElsewhere("birdSpecies", sim)){
     sim$birdSpecies <- c("BBWA", "BLPW", "BOCH", "BRCR", 
                          "BTNW", "CAWA", "CMWA", "CONW", 
                          "OVEN", "PISI", "RBNU", "SWTH", 
-                         "TEWA", "WETA", "YRWA")
-    }
+                         "TEWA", "WETA", "YRWA")}
   
   if (!suppliedElsewhere(sim$typeDisturbance)){
     sim$typeDisturbance = c("Transitional", "Permanent", "Both")
   }
-
+  
   if (!suppliedElsewhere("disturbanceDimension", sim)){
     sim$disturbanceDimension = c("local", "neighborhood", "LocalUndisturbed")
   }
   
-  if (!suppliedElsewhere("SQLtableVersion", sim)){
-    sim$SQLtableVersion <- "V4_2015"
-  }
-  if (!suppliedElsewhere("SQLServer", sim)){
-    sim$SQLServer = "boreal.biology.ualberta.ca"
-  }
-  if (!suppliedElsewhere("SQLDatabase", sim)){
-    sim$SQLDatabase = "BAM_National_V4_2015_0206"
-  }
-  
-return(invisible(sim))
+  return(invisible(sim))
 }
