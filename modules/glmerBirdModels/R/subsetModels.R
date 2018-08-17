24fa4f7d07f9dbbc752e92ba6f3350433fbeeab1
tati-micheletti <tati.micheletti@gmail.com>
2018-08-17 11:30:23 -0700

    fixed bug in subseModels() related to focal distance

--- modules/glmerBirdModels/R/subsetModels.R
+++ modules/glmerBirdModels/R/subsetModels.R
@@ -5,11 +5,11 @@ subsetModels <- function(disturbancePredict = sim$disturbancePredict,
   if(any(grepl(pattern = "focalDistance", names(unlist(prmt))))){
     rw <- which(grepl(pattern = "focalDistance", names(unlist(prmt))))
     focalDistance <- as.numeric(unlist(prmt)[rw])
-    if(!focalDistance %in% c(100, 500)){
+    if(all(!focalDistance %in% c(100, 500))){
       warning("Predictions won't be performed for focal distances other than 100 or 500")
     }
-    comb <- if (focalDistance == 100) paste0("local", disturbancePredict) else 
-              if (focalDistance == 500) paste0("neighborhood", disturbancePredict) else NULL
+    comb <- if (all(length(focalDistance) == 1 & focalDistance == 100)) paste0("local", disturbancePredict) else 
+              if (all(length(focalDistance) == 2 & focalDistance == c(100, 500))) paste0("neighborhood", disturbancePredict) else NULL
     slct <- which(names(models) %in% comb)
     models <- models[[slct]]
   }
