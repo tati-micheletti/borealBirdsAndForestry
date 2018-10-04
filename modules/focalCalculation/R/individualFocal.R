e04be783e6f8848ec81230ec7cad4b29835286ff
tati-micheletti <tati.micheletti@gmail.com>
2018-10-04 10:28:18 -0700

    Hijacked Ian's individualFocal() from SplitModelPlot module

---
+++ modules/focalCalculation/R/individualFocal.R
@@ -0,0 +1,9 @@
+individualFocal <- function(inList, inWeight, denomRas) {
+
+  a <- raster::focal(inList, w = inWeight, na.rm = TRUE)
+  b <- overlay(x = a, y = denomRas, fun = function(x,y) {return(x/y)})
+  b[is.infinite(b)] <- 0 #some inf values returned from dividing by 0 (non-forest in LCC)
+
+  return(b)
+
+}
