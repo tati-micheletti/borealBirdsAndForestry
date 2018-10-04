8f18cf4fbd19d0fbbc65c35610068ffbc35a5496
tati-micheletti <tati.micheletti@gmail.com>
2018-10-03 21:56:18 -0700

    applyFocalToTiles() working with fake data Removed not used arguments, commented possible ones to use when using real data finished script

--- modules/focalCalculation/R/applyFocalToTiles.R
+++ modules/focalCalculation/R/applyFocalToTiles.R
@@ -1,16 +1,15 @@
-applyFocalToTiles <- function(useParallel = P(sim)$useParallel, # Should do paralell only for focal and predicting, maybe?
+applyFocalToTiles <- function(#useParallel = P(sim)$useParallel, # Should do paralell only for focal and predicting, maybe?
                                   listTilePaths = sim$listTilePaths,
-                                  pathData = dataPath(sim),
-                                  pathCache = cachePath(sim), 
-                                  startTime = start(sim), 
-                                  endTime = end(sim),
+                                  # pathData = dataPath(sim),
+                                  # pathCache = cachePath(sim), 
                                   forestClass = P(sim)$forestClass,
                                   focalDistance = P(sim)$focalDistance,
                                   disturbanceClass = P(sim)$disturbanceClass,
                                   recoverTime = P(sim)$recoverTime,
-                                  ressampledRes = P(sim)$ressampledRes){
+                                  resampledRes = P(sim)$resampledRes,
+                                  currentYear = time(sim)){
   
-  browser()
+#  [ FIX ] Add parallel for tile masking!!!
   
 # Subset matching tiles
   message(crayon::green("Tiles organized..."))
@@ -22,86 +21,142 @@ applyFocalToTiles <- function(useParallel = P(sim)$useParallel, # Should do para
   })
   names(orderedRasterList) <- paste0("tile", lengthVector)
   
+  message(crayon::red(paste0("Starting masking for year ",
+                             currentYear, " (Time: "
+                             , Sys.time(), ")")))
+  
   # Entering each tile group
-  reprojectedAssambledTiles <- lapply(X = lengthVector, FUN = function(tiles){
-    message(crayon::red(paste0("Starting nested functions on ", 
-                               names(orderedRasterList)[tiles], 
-                               " of ", totalTiles)))
-    # VERSION BEFORE REMODELING. WORKFLOW BELOW WON'T WORK...
-    assign(x = paste0(names(orderedRasterList)[tiles]),
-           value = lapply(X = 1:length(listTilePaths), FUN = function(eachTile) {
-             ras <- raster::raster(orderedRasterList[[tiles]][eachTile])
-             rasterName <- ras@data@names
-             ras[] <- ras[] %>% # Bring raster to memory, faster processing
-               round(0)  # Round to 0, useful for integer rasters, makes them smaller
-             storage.mode(ras[]) <- "integer" # Makes rasters smaller
-             
-             if (eachTile == 1) {
-               # TILES 1 TYPE
-               binaryDisturb <- binaryReclassify(inFile = ras, inValues = disturbanceClass)
-               storage.mode(binaryDisturb[]) <- "integer" # Reducing size of raster by converting it to a real binary
-               
-             } else {
-               if (eachTile == 2) {
-                 # TILES 2 LC
-                 binaryLC <- binaryReclassify(inFile = ras, inValues = forestClass)
-                 storage.mode(binaryLC[]) <- "integer" # Reducing size of raster by converting it to a real binary
-                 
-               } else {
-                 # TILES 3 YEAR
-               }
-             }
-             
-             return(ras)
-           }
-             )
-           )
-  })
-    tileName <- get(names(orderedRasterList)[tiles])
-    tileStack <- raster::stack(x = tileName) # Dive into Ian's functions. There is the matter of focal in areas that surround non-forest, etc... 
-    #  Bring his functions here. As I have to work with these in memory and I obviously can't bring all of them to memory... 
+  focalTilesToMerge <- lapply(X = lengthVector, FUN = function(tiles){
+      
+# =============== RASTER 1 (in this case, disturbance TYPE raster) =============== #
     
-    browser()
+             Raster1 <- raster::raster(orderedRasterList[[tiles]][1])
+             Raster1[] <- Raster1[] %>% # Bring raster to memory, faster processing
+               round(0)  # Round to 0, useful for integer rasters, makes them smaller
+             storage.mode(Raster1[]) <- "integer" # Reducing size of raster by converting it to a real binary
+             
+      if (!all(is.na(Raster1[])) == TRUE) {
+
+             # 1. Converting raster to binary to select only harvesting disturbances
+             binaryRaster1 <- binaryReclassify(inFile = Raster1, inValues = disturbanceClass)
+             rm(Raster1)
+             gc()
+             
+             # binaryRaster1 = binaryDisturb
+             binaryRaster1[] <- binaryRaster1[] %>% # Bring raster to memory, faster processing
+               round(0)  # Round to 0, useful for integer rasters, makes them smaller
+               storage.mode(binaryRaster1[]) <- "integer" # Reducing size of raster by converting it to a real binary
+               
+               if (max(binaryRaster1[]) == 0) {
+                 message(crayon::yellow(paste0("Year ", currentYear, " for ", names(orderedRasterList)[tiles], 
+                                               " was skipped as it doesn't have activities type ", disturbanceClass)))
+                 Raster3 <- binaryRaster1
+                 
+                 # Resample raster to 250m
+                 y <- raster::raster(res = c(resampledRes, resampledRes), 
+                                     crs = raster::crs(Raster3),
+                                     ext = extent(Raster3))
+                 Raster3 <- raster::resample(x = Raster3, y = y, method = "ngb")
+                 return(Raster3)
+               }
+               
+# =============== RASTER 2 (in this case, LAND COVER raster) =============== #
+               
+               Raster2 <- raster::raster(orderedRasterList[[tiles]][2])
+               Raster2[] <- Raster2[] %>% # Bring raster to memory, faster processing
+                 round(0)  # Round to 0, useful for integer rasters, makes them smaller
+               storage.mode(Raster2[]) <- "integer" # Reducing size of raster by converting it to a real binary
+               
+               # 1. Converting raster to binary to select only harvesting disturbances
+               binaryRaster2 <- binaryReclassify(inFile = Raster2, inValues = forestClass)
+               rm(Raster2)
+               gc()
+               
+               binaryRaster2[] <- binaryRaster2[] %>% # Bring raster to memory, faster processing
+                 round(0)  # Round to 0, useful for integer rasters, makes them smaller
+               storage.mode(binaryRaster2[]) <- "integer" # Reducing size of raster by converting it to a real binary
+               
+               # 2. create focal matrix (for each distance parameter). 
+               #if annulus = TRUE, it will use the largest and smallest distances to make the annulus (you should only have 2). One day we will adapt this to work with multiple buffers concurrently 
+               if(length(focalDistance)> 1) {
+                 #make inner matrix
+                 inMat <-
+                   raster::focalWeight(x = binaryRaster2, d = min(focalDistance))
+                 outMat <-
+                   raster::focalWeight(x = binaryRaster2, d = max(focalDistance))
+                 #inverse the inner matrix
+                 inMat[inMat == 0] <- 1
+                 inMat[inMat < 1] <- 0
+                 #Get dimensions for matrix
+                 innerDim <- floor(dim(inMat)[1] / 2)
+                 outerDim <- ceiling(dim(outMat)[1] / 2)
+                 #Merge the two matrices
+                 outMat[(outerDim - innerDim):(outerDim + innerDim), 
+                        (outerDim - innerDim):(outerDim + innerDim)] <- inMat
+                 #Recalculate the matrix value as 1/sum of non-zero values
+                 outMat[outMat > 0] <- 1 / length(outMat[outMat > 0])
+                 focalMatrices <- list(outMat)
+               } else {
+                 focalMatrices <-
+                   lapply(focalDistance, FUN = focalWeight, x = binaryRaster2)
+               }
+               
+               # 3. calculate focal statistics with each matrix
+               LCFocals <- lapply(focalMatrices, FUN = raster::focal, x = binaryRaster2, na.rm = TRUE)
+               rm(binaryRaster2) # Remove binaryRaster2 so we free memory for the next raster to be processed
+               gc()
+
+# =============== RASTER 3 (in this case, disturbance YEAR raster) =============== #
+               
+               Raster3 <- raster::raster(orderedRasterList[[tiles]][3])
+               Raster3[] <- Raster3[] %>% # Bring raster to memory, faster processing
+                 round(0)  # Round to 0, useful for integer rasters, makes them smaller
+               storage.mode(Raster3[]) <- "integer" # Reducing size of raster by converting it to a real binary
+               
+               if (currentYear > 1000){
+                 currentYear <- currentYear - 1900
+               }
+               
+               # Calculate cummulative effects. If not wanted, change to maskValue <- currentYear
+               maskValue <- c((currentYear - recoverTime) : currentYear)
+               
+               # Replace values of interest for big ones (999)
+               yearValue <- getValues(Raster3)
+               yearValue[yearValue %in% maskValue] <- 999
+               Raster3 <- setValues(Raster3, yearValue)
+               
+               message(crayon::yellow(paste0("Masking ", names(orderedRasterList)[tiles],
+                                             " of ", totalTiles, " tiles (Time: "
+                                             , Sys.time(), ")")))
+               
+               Raster3 <- raster::mask(x = binaryRaster1, mask = Raster3, maskvalue = 999, inverse = TRUE, updatevalue = 0)
+               
+               # Resample raster to 250m
+               y <- raster::raster(res = c(resampledRes, resampledRes), 
+                                   crs = raster::crs(Raster3),
+                                   ext = extent(Raster3))
+               Raster3 <- raster::resample(x = Raster3, y = y, method = "ngb")
+               
+              return(Raster3)
+  } else {
+    message(crayon::red(paste0(tileNumber, 
+                               " was skipped as it has only NA values (Time: ", Sys.time(), ")")))
+    y <- raster::raster(res = c(resampledRes, resampledRes), 
+                        crs = raster::crs(binaryRaster1),
+                        ext = extent(binaryRaster1))
+    Raster3 <- raster::resample(x = binaryRaster1, y = y, method = "ngb")
+    return(binaryRaster1)
+  }
+  })
+
+  gc()
+  # IF FAILS (ALL 250m RASTERS ARE IN MEMORY, ADD filename ARGUMENT TO resample() so it writes to disk)
+  # mergedFocalTiles <- SpaDES.tools::mergeRaster(focalTilesToMerge) # NOT SURE IT WORKS WITH RASTERS IN MEMORY UNCOMMENT [ FIX ]
+  gc()
   
+  mergedFocalTiles <- focalTilesToMerge[[1]]  # DUMMY LINE EXCLUDE [ FIX ]
+  mergedFocalTiles@data@names <- paste0("Year", currentYear)
   
-  # #Old function below
-  # #=============================================================
-  # 
-  # 
-  # browser() # TRY BRINGING ALL 3 INTO MEMORY. 
-  # sim$Raster1[] <- round(sim$Raster1[], 0)
-  # storage.mode(sim$Raster1[]) = "integer"
-  # 
-  # tileNumber <- x 
-  # 
-  # #Subset corresponding tiles 
-  # tilelist <- lapply(inList, '[[', x)
-  # names(tilelist) <- origList
-  # 
-  # if (!all(is.na(tilelist$birdDensityRasters[])) == TRUE){
-  #   
-  #   #reclassify and mask them (and run focal distances)
-  #   # processed <- LCReclassify(inputTiles = tilelist, 
-  #   #                           pathData = pathData, 
-  #   #                           pathCache = pathCache, 
-  #   #                           startTime = startTime,
-  #   #                           endTime = endTime,
-  #   #                           forestClass = forestClass,
-  #   #                           focalDistance = focalDistance,
-  #   #                           disturbanceClass = disturbanceClass,
-  #   #                           spName = spName,
-  #   #                           passedModel = passedModel,
-  #   #                           tileNumber = tileNumber,
-  #   #                           recoverTime = recoverTime,
-  #   #                           maxTile = maxTile)
-  #   
-  #   return(processed)
-  #   
-  # } else {
-  #   
-  #   message(crayon::red(paste0("Tile ",  tileNumber, " of ",
-  #                              maxTile, " tiles for ", spName,
-  #                              " was skipped as it has only NA values (Time: ", Sys.time(), ")")))
-  #   
-  # }
+  return(mergedFocalTiles)
 }
+
