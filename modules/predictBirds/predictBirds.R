10f4c6b969391710a4766a7fa8f1496b33d1e53d
Tati Micheletti <tati.micheletti@gmail.com>
2018-10-23 16:42:49 -0700

    predictBirds :: added .inputObjects

--- modules/predictBirds/predictBirds.R
+++ modules/predictBirds/predictBirds.R
@@ -12,14 +12,17 @@ defineModule(sim, list(
   timeunit = "year",
   citation = list("citation.bib"),
   documentation = list("README.txt", "predictBirds.Rmd"),
-  reqdPkgs = list(),
+  reqdPkgs = list("raster"),
   parameters = rbind(
     #defineParameter("paramName", "paramClass", value, min, max, "parameter description"),
-    defineParameter(".plotInitialTime", "numeric", NA, NA, NA, "This describes the simulation time at which the first plot event should occur"),
-    defineParameter(".plotInterval", "numeric", NA, NA, NA, "This describes the simulation time interval between plot events"),
-    defineParameter(".saveInitialTime", "numeric", NA, NA, NA, "This describes the simulation time at which the first save event should occur"),
-    defineParameter(".saveInterval", "numeric", NA, NA, NA, "This describes the simulation time interval between save events"),
-    defineParameter(".useCache", "logical", FALSE, NA, NA, "Should this entire module be run with caching activated? This is generally intended for data-type modules, where stochasticity and time are not relevant")
+    defineParameter("focalDistance", "numeric", 100, NA, NA, 
+                    paste0("The distance at which to compute focal statistics, in units of", 
+                           " the input rastesr CRS. This will be used to ", 
+                           "create a matrix with circular weights summing to 1)")),
+    defineParameter(".useCache", "logical", FALSE, NA, NA, 
+                    paste0("Should this entire module be run with caching activated?",
+                           " This is generally intended for data-type modules, where ",
+                           "stochasticity and time are not relevant"))
   ),
   inputObjects = bind_rows(
     expectsInput(objectName = "focalYearList", objectClass = "list", 
@@ -27,37 +30,39 @@ defineModule(sim, list(
                                 "masked value passed is the first level of ",
                                 "the list, which is composed of a full patched",
                                 " raster with all merged tiles")),
-    expectsInput(objectName = "counter", objectClass = "numeric",
-                  desc = paste0("Counter of the number of tiles",
-                                " used for saving the focal list")),
     expectsInput(objectName = "birdSpecies", objectClass = "character", 
                  desc = "List of bird species to be modeled"),
     expectsInput(objectName = "models", objectClass = "list", 
                  desc = "list of boreal bird models"),
     expectsInput(objectName = "birdDensityRasters", objectClass = "list", 
                  desc = paste0("list of rasters with information",
-                               " on species densities based on LCC and BCR/Prov"))
+                               " on species densities based on LCC and BCR/Prov")),
+    expectsInput(objectName = "focalDistance", objectClass = "numeric", 
+                 desc = paste0("Distance for which the disturance % is calculated"))
   ),
   outputObjects = bind_rows(
     createsOutput(objectName = NA, objectClass = NA, desc = NA)
   )
 ))
 
-## event types
-#   - type `init` is required for initialiazation
-
 doEvent.predictBirds = function(sim, eventTime, eventType) {
   switch(
     eventType,
     init = {
       
-      # Check if the rasters align (focalYearList[[x]] and birdDensityRasters[[x]]) [ FIX ]
-     
       # schedule future event(s)
-      sim <- scheduleEvent(sim, P(sim)$.plotInitialTime, "predictBirds", "plot")
+      sim <- scheduleEvent(sim, time(sim), "predictBirds", "predictBirdsDensities")
     },
-    plot = {
-
+    predictBirdsDensities = {
+      
+      sim$predictRas[[paste0("Year", time(sim))]] <- predictDensities(birdSpecies = sim$birdSpecies,
+                                                                      disturbanceRas = sim$focalYearList[[paste0("Year", time(sim))]],
+                                                                      birdDensityRasters = sim$birdDensityRasters,
+                                                                      currentTime = time(sim),
+                                                                      models = sim$models)
+      
+      # schedule future event(s)
+      sim <- scheduleEvent(sim, time(sim) + 1, "predictBirds", "predictBirdsDensities")
       
     },
     warning(paste("Undefined event type: '", current(sim)[1, "eventType", with = FALSE],
@@ -67,6 +72,75 @@ doEvent.predictBirds = function(sim, eventTime, eventType) {
 }
 
 .inputObjects <- function(sim) {
-
+  if (!suppliedElsewhere("focalYearList", sim)){
+    message(paste0("No disturbance list of rasters found.", 
+                   "Using fake disturbance raster for years ", 
+                   start(sim), ":", end(sim)))
+    sim$focalYearList <- fakeFocalRasterYears(st = start(sim),
+                                              ed = end(sim),
+                                              res = c(250, 250),
+                                              crsRas = "+proj=utm +zone=15 +ellps=GRS80 +datum=NAD83 +units=m +no_defs")
+    names(sim$focalYearList) <- paste0("Year", start(sim):end(sim))
+  }
+  if (!suppliedElsewhere("birdDensityRasters", sim)){
+    if (suppliedElsewhere("birdSpecies", sim)){
+      stop(paste0("Bird list supplied, but density rasters not.",
+                  " Please either provide either both objects, or ",
+                  "none of these two."))
+    } else {
+    message(paste0("No bird density raster found.", 
+                   "Using fake density raster for year BBWA"))
+    sim$birdDensityRasters <- list()
+    suppressWarnings(dir.create(file.path(dataPath(sim), "tmp")))
+    tmpDir <- file.path(dataPath(sim), "tmp/tempBirdDensityRas.tif")
+    if (!file.exists(tmpDir)) {
+     ras <- SpaDES.tools::gaussMap(raster::raster(xmn = -30^3, xmx = 30^3,
+                                            ymn = -30^3, ymx = 30^3,
+                                            resolution = c(250, 250)))
+      raster::crs(ras) <- "+proj=utm +zone=15 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
+      ys <- seq(from = 0.6, to = 0.9, by = 0.05)
+      bff <- (max(ras[]) - min(ras[]))/length(ys)
+      vecM <- numeric(length(ys))
+      cnty <- 1
+      mult <- 0
+      for (i in seq(from = 1, to = length(ys)*3, by = 3)){
+        vecM[i:(2+i)] <- c(bff*mult, bff*(mult+1), ys[cnty])
+        mult <- mult + 1
+        cnty <- cnty + 1
+      }
+      rclmat <- matrix(vecM, ncol=3, byrow=TRUE)
+      ras <- raster::reclassify(x = ras, rcl = rclmat)
+    raster::writeRaster(x = ras, filename = tmpDir, format = "GTiff", overwrite = TRUE)
+    }
+    sim$birdDensityRasters[["BBWA"]] <- tmpDir
+    }
+  }
+  if (!suppliedElsewhere("models", sim)){
+    message("No models list found. Using fake generalized linear model for bay-breasted warbler (BBWA)")
+    data <- data.frame(structure(list(AB_BBWA = c(0L, 0L, 0L, 0L, 0L, 1L, 0L, 0L, 0L, 
+                                                  0L, 1L, 1L, 0L, 1L, 0L, 0L, 0L, 2L, 0L), 
+                                      State_P_100 = c(1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 
+                                                      1, 1, 0.039601406, 0.039601406, 0.039601406, 
+                                                      0.00896732), 
+                                      OFFSET_BBWA = c(0.143327645, 0.111694189, 0.122903008, 0.166319377, 
+                                                  0.172458391, 0.159933748, 0.146178298, 0.159437289, 
+                                                  0.144909933, 0.080126266, 0.101474693, 0.134386042, 
+                                                  0.066678816, 0.148570951, 0.130694053, 0.098123795, 
+                                                  0.139348905, 0.15018566, 0.144155741), 
+                                      logDENSITY_BBWA = c(-0.300024604, -0.300024604, -0.300024604, -0.300024604, -0.300024604, 
+                                                          -0.300024604, -0.155355509, -0.155355509, -0.155355509, -0.300024604, 
+                                                          -0.300024604, -0.300024604, -0.300024604, -0.300024604, -0.300024604, 
+                                                          -0.300024604, -0.300024604, -0.300024604, -0.300024604)), 
+                                 .Names = c("AB_BBWA", "State_P_100","OFFSET_BBWA", "logDENSITY_BBWA"), 
+                                 class = "data.frame", row.names = c(NA, -19L)))
+    sim$models <- glm(AB_BBWA ~ State_P_100 + logDENSITY_BBWA, 
+                      offset = OFFSET_BBWA, 
+                      family = 'poisson', 
+                      data = data)
+    if (!suppliedElsewhere("birdSpecies", sim)){
+      message("No species list found. Using fake data for bay-breasted warbler (BBWA)")
+      sim$birdSpecies <- "BBWA"
+    }
+  }
   return(invisible(sim))
 }
