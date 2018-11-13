61a3b56a79c64be89b244c9d54e367212c896602
tati-micheletti <tati.micheletti@gmail.com>
2018-11-13 11:48:32 -0800

    Updated globalRemodeled.R

--- globalRemodeled.R
+++ globalRemodeled.R
@@ -1,16 +1,21 @@
 # Global script for the Backcasting Project REMODELED
 
-# OBS. on 23rd Oct I installed tati-micheletti/SpaDES.tools 
-# ref = f04ed7183dfb038f4278fa1654ee0c065c2908c6 as my PR is still being analyzed by Travis
-# This looks ready, but still need to test it. On 23rd Eliot asked me to use 388 for a few days, 
-# so I will wait until they are finished
+# OBS. on 26th Oct I installed back PredictiveEcology/SpaDES.tools 
+# ref = 42a02a10b61cf5dfef557d5c50e60477fc3ab74c (Which includes my changes to mergeRaster)
+# OBS. on 29th Oct I installed tati-micheletti/reproducible 
+# ref = daccc226839b2b7970e6af6721126f91bd806dce (Which includes my fix to preProcess / guessAtFile)
+
+# HAVE TO RE-WRITE THE CODE TO INCLUDE IN THE FINAL MERGED FILES THE NAME OF THE focal distance!!!
+
+# FOR 500m
+# DOUBLE CHECK IF FOCAL WEIGHT WILL GET THE CACHE. IT SHOULD NOT!
 
 library(SpaDES.core)
 library(SpaDES.tools)
-# tryCatch(library(unixtools), 
-#          error = function(e) install.packages("unixtools", repos = 'http://www.rforge.net/'))
-# options("reproducible.useMemoise" = FALSE) # Avoids bringing cache to memory
-# unixtools::set.tempdir("/mnt/storage/temp")
+tryCatch(library(unixtools),
+         error = function(e) install.packages("unixtools", repos = 'http://www.rforge.net/'))
+options("reproducible.useMemoise" = FALSE) # Avoids bringing cache to memory
+unixtools::set.tempdir("/mnt/storage/temp")
 
 
 # set the directories
@@ -18,22 +23,18 @@ workDirectory <- getwd()
 
 paths <- list(
   # As the project takes up a LOT of space, all mid steps will be saved inside the cache folder of another partition,
-  # cachePath = file.path("/mnt/storage/borealBirdsAndForestry", "cache"),
-  cachePath = file.path(workDirectory, "cache"),
+  cachePath = file.path("/mnt/storage/borealBirdsAndForestry", "cache"),
   # while the other folders are in the working directory
   modulePath = file.path(workDirectory, "modules"),
   inputPath = file.path(workDirectory, "inputs"),
   outputPath = file.path(workDirectory, "outputs")
 )
 
+options("reproducible.cachePath" = paths$cachePath)
 setPaths(modulePath = paths$modulePath, inputPath = paths$inputPath, outputPath = paths$outputPath, cachePath = paths$cachePath)
 
-# if (dir.exists("/mnt/storage/borealBirdsAndForestry/cache/outputRasters")){ # Delete all previous tiles so we have only the most updated ones
-#   unlink(file.path(paths$cachePath, "outputRasters"), recursive = TRUE)
-# }
-
 ## list the modules to use
-modules <- list("birdAbundanceTrends")
+modules <- list("birdDensityBCR_Prov_LCC", "loadOffsetsBAM", "glmerBirdModels", "prepTiles", "focalCalculation")
 #Complete set of modules: "birdDensityBCR_Prov_LCC", "loadOffsetsBAM", "glmerBirdModels", "prepTiles", 
                          #"focalCalculation", "predictBirds", "birdAbundanceTrends", "finalRasterPlots
 
@@ -60,37 +61,37 @@ parameters <- list(
                            useParallel = NULL) # "across" = across machines, "local" = only on local machine, "NULL" or anything else = no parallel
         )
 
-# objects <- list( # Possible to include 'rP' directly here as a shapefile!
-#   mapSubset = NULL, # Provinces to run at once. Good to subset provinces still within the boreal
-#   specificTestArea = "boreal", # "boreal",
-#   SQLtableVersion = "V4_2015", # Data retrieving from SQL: specific versions
-#   SQLServer = "boreal.biology.ualberta.ca", # Data retrieving from SQL: server
-#   SQLDatabase = "BAM_National_V4_2015_0206", # Data retrieving from SQL: specific database
-#   dataName = "Final_points_2010.csv", # Alberto's manuscript data to select points. Data are, however coming from SQL.
-#   birdSpecies = c("BBWA", # Bird species to run the models for
-#                   #"BLPW"#,
-#                   # "BOCH",
-#                   # "BRCR",
-#                   "BTNW",
-#                   "CAWA",
-#                   "CMWA"#,
-#                   # "CONW",
-#                   # "OVEN",
-#                   # "PISI",
-#                   # "RBNU",
-#                   # "SWTH",
-#                   # "TEWA",
-#                   # "WETA",
-#                   # "YRWA"
-#   ),
-#   typeDisturbance = c("Transitional", "Permanent", "Both"), #, "Permanent", "Both"
-#   disturbanceDimension = c("local", "neighborhood", "LocalUndisturbed"), #, "neighborhood", "LocalUndisturbed"
-#   disturbancePredict = c("Transitional")#, # Needs to match disturbanceClass from prediction module. Type of disturbance we want to predict from.
-#   # Raster1 = NA, # direct path to locally stored object as a "character string.
-#   # urlRaster1 = NA, #If you want to download the object from a specific url that not the default, specify the url as a "character string" here
-#   # Use the same logic for Raster2 and Raster3
-# )
-objects <- list()
+objects <- list( # Possible to include 'rP' directly here as a shapefile!
+  mapSubset = NULL, # "Canada" or Provinces to run at once. Good to subset provinces still within the boreal
+  specificTestArea = "boreal", # "boreal",
+  SQLtableVersion = "V4_2015", # Data retrieving from SQL: specific versions
+  SQLServer = "boreal.biology.ualberta.ca", # Data retrieving from SQL: server
+  SQLDatabase = "BAM_National_V4_2015_0206", # Data retrieving from SQL: specific database
+  dataName = "Final_points_2010.csv", # Alberto's manuscript data to select points. Data are, however coming from SQL.
+  birdSpecies = c("BBWA", # Bird species to run the models for
+                  #"BLPW"#,
+                  # "BOCH",
+                  # "BRCR",
+                  "BTNW",
+                  "CAWA",
+                  "CMWA"#,
+                  # "CONW",
+                  # "OVEN",
+                  # "PISI",
+                  # "RBNU",
+                  # "SWTH",
+                  # "TEWA",
+                  # "WETA",
+                  # "YRWA"
+  ),
+  typeDisturbance = c("Transitional", "Permanent", "Both"), #, "Permanent", "Both"
+  disturbanceDimension = c("local", "neighborhood", "LocalUndisturbed"), #, "neighborhood", "LocalUndisturbed"
+  disturbancePredict = c("Transitional")#, # Needs to match disturbanceClass from prediction module. Type of disturbance we want to predict from.
+  # Raster1 = NA, # direct path to locally stored object as a "character string.
+  # urlRaster1 = NA, #If you want to download the object from a specific url that not the default, specify the url as a "character string" here
+  # Use the same logic for Raster2 and Raster3
+)
+
 clearPlot()
 
 #file.remove("/mnt/storage/borealBirdsAndForestry/cache/logParallel")
