e4c9c80e2be6f7a5dd119be5fc5e0768ca6e4f44
tati-micheletti <tati.micheletti@gmail.com>
2018-11-19 14:13:54 -0800

    Now rasters for prediction should be fixed if cannot be stacked

--- modules/predictBirds/R/predictDensities.R
+++ modules/predictBirds/R/predictDensities.R
@@ -3,31 +3,48 @@ predictDensities <- function(birdSpecies = sim$birdSpecies,
                              birdDensityRasters = sim$birdDensityRasters,
                              currentTime = time(sim),
                              modelList = sim$models,
-                             pathData = dataPath(sim)) {
+                             pathData = cachePath(sim)) {
 
-predictionPerSpecies <-  lapply(birdSpecies, function(spName){
+predictionPerSpecies <-  lapply(birdSpecies, function(spName){ # I can paralellize the predictions up to 3 times safely apparently. It in only consuming 16Gb per species
   message(crayon::yellow("Stacking rasters for ", spName , " prediction"))
+  suppressWarnings(dir.create(file.path(pathData, "predicted")))
+  models <- modelList[[spName]]
+  if ("glmerMod" %in% class(models)){
+    nameStackRas1 <- names(models@frame)[2]
+    nameStackRas2 <- names(models@frame)[3]
+  } else {
+    if ("glm" %in% class(models)){
+      nameStackRas1 <- names(models$coefficients)[2]
+      nameStackRas2 <- names(models$coefficients)[3]
+    }
+  }
+  focDis <- as.numeric(gsub("[^\\d]+", "", nameStackRas1, perl=TRUE))
+  predictedName <- file.path(pathData, paste0("predicted/predictedFocal", focDis, "m", spName, currentTime, ".tif"))
+  if (!file.exists(predictedName)){
   birdD <- raster::raster(birdDensityRasters[[spName]])
   valsD <- log(raster::getValues(birdD)) # log the value of densities so it is the same of the original model
-  valsD[valsD < -0.9] <- -1
+  valsD[valsD < -0.99] <- -1
   birdD <- raster::setValues(birdD, valsD)
   rm(valsD)
   gc()
-  stackRas <- raster::stack(disturbanceRas, birdD) # Might need to remove individual rasters here
-  models <- modelList[[spName]]
-  if ("glmerMod" %in% class(models)){
-    names(stackRas)[1] <- names(models@frame)[2]
-    names(stackRas)[2] <- names(models@frame)[3]
-  } else {
-    if ("glm" %in% class(models)){
-      names(stackRas)[1] <- names(models$coefficients)[2]
-      names(stackRas)[2] <- names(models$coefficients)[3]
-    }
+  if (any(!identical(round(raster::extent(x = disturbanceRas), 10^-100), round(raster::extent(birdD), 10^-100)),
+          !identical(raster::res(x = disturbanceRas), raster::res(birdD)),
+          !identical(raster::crs(x = disturbanceRas), raster::crs(birdD)))){
+    disturbanceRas <- postProcess(x = disturbanceRas, rasterToMatch = birdD,
+                                 maskWithRTM = TRUE, 
+                                 filename2 = file.path(pathData, "predicted", paste0(disturbanceRas@data@names, "Fixed")),
+                                 format = "GTiff", overwrite = TRUE, useCache = FALSE)
   }
-  predicted <- fitModel(inRas = stackRas, inputModel = models, spName = spName, tileYear = currentTime)
-  suppressWarnings(dir.create(file.path(pathData, "predicted")))
-  predictedName <- file.path(pathData, paste0("predicted/predicted", spName, currentTime, ".tif"))
-  raster::writeRaster(x = predicted, filename = predictedName, format = "GTiff", overwrite = TRUE)
+  stackRas <- raster::stack(disturbanceRas, birdD) # Might need to remove individual rasters here
+    names(stackRas)[1] <- nameStackRas1
+    names(stackRas)[2] <- nameStackRas2
+  suppressWarnings(predicted <- fitModel(inRas = stackRas, 
+                                         inputModel = models, 
+                                         spName = spName, 
+                                         tileYear = currentTime))
+  raster::writeRaster(x = predicted, filename = predictedName, 
+                      format = "GTiff", overwrite = TRUE)
+  }
   predicted <- raster(predictedName)
   gc()
   return(predicted) # The predicted value is NOT multiplied by 1000! For that, need to change fitModel!
