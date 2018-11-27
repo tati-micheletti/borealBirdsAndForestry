15b1168e4afd1fc98f772aa0753f2bb0631d15b8
tati-micheletti <tati.micheletti@gmail.com>
2018-11-27 09:18:06 -0800

    Added a todo2

---
+++ ToDo2.R
@@ -0,0 +1,11 @@
+# TO DO:
+
+# If all good, can start the 500m.
+# Need to test the new BCR_PROV branch. If working, merge to remodeledOrder
+# Then fix all stuff on globalRemodeled
+# Then put to run the 500m and hope for the best! ehehehe.
+# Need to: 
+# 1. Start again the manuscript, at least methods
+# 2. Prepare slides for the presentation. When the current 100m are done, 
+# I should be able to show the maps in the slides. Focus on what should be the story telling. 
+# For that also CHECK my e.mail to myself! Lots of goodies there
