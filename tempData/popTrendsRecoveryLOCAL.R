e38d7ed497310ac194ae3bea05c3abb8845e951b
Tati Micheletti <tati.micheletti@gmail.com>
2018-12-06 18:48:23 +0000

    clean up BC

--- tempData/popTrendsRecoveryLOCAL.R
+++
@@ -1,3 +0,0 @@
-pathData <- file.path(getwd(), "modules", "finalRasterPlots", "data")
-outputPath <- "/home/tmichele/Documents/GitHub/borealBirdsAndForestry/outputs"
-percDensityChange <- slopeDensityGraph(populationTrends = mySimOut@.envir$populationTrends, studyArea = mySimOut@.envir$rP, pathData = pathData, outputPath = outputPath)
