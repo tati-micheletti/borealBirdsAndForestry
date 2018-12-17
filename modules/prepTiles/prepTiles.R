f578424d417d3ac9ec7e0e353375beb533932d48
Tati Micheletti <tati.micheletti@gmail.com>
2018-12-17 23:34:34 +0000

    adjusting prepTiles to borealCloud, added defineStudyArea() function

--- modules/prepTiles/prepTiles.R
+++ modules/prepTiles/prepTiles.R
@@ -92,29 +92,30 @@ doEvent.prepTiles = function(sim, eventTime, eventType) {
                                     " the extent of the raster.",
                                     " (Time: "
                                     , Sys.time(), ")")))
+
       sim$Raster1 <- Cache(prepInputs, url = sim$urlRaster1,
-                                 destinationPath = dataPath(sim),
-                                 studyArea = sim$rP,
-                                 length = TRUE, useCache = Inf,
-                                 userTags = c("objectName:Raster1",
-                                              "fileName:C2C_change_type"))#,
- #                          cacheId = "74286256127df354937f3d89fe187a4a") # length = TRUE digests the whole 
+                           destinationPath = dataPath(sim),
+                           studyArea = sim$rP,
+                           length = TRUE, useCache = Inf,
+                           filename2 = "Raster1.tif",
+                           userTags = c("objectName:Raster1",
+                                        "fileName:C2C_change_type"))#,
+      #                          cacheId = "74286256127df354937f3d89fe187a4a") # length = TRUE digests the whole 
       gc()
-      
       message(crayon::yellow(paste0("Raster2 being prepared.",
                                     " This might take a few hours depending on",
                                     " the extent of the raster.",
                                     " (Time: "
                                     , Sys.time(), ")")))
-      sim$Raster2 <- Cache(prepInputs, #url = sim$urlRaster2, # Will restablish the url as soon as prepInputs is working with .rar again
-                            targetFile = "CAN_NALCMS_LC_30m_LAEA_mmu12_urb05.tif",
-                            destinationPath = dataPath(sim),
-                            rasterToMatch = sim$Raster1,
-                            studyArea = sim$rP,
-                            length = Inf, useCache = TRUE,
-                            userTags = c("objectName:Raster2", 
-                                         "fileName:CAN_NALCMS_LC_30m_LAEA_mmu12_urb05"))#,
-                           # cacheId = "af8ac2b611698d718c31a8f339ad1f07")
+      sim$Raster2 <- Cache(prepInputs, url = sim$urlRaster2,
+                           targetFile = "CAN_NALCMS_LC_30m_LAEA_mmu12_urb05.tif",
+                           destinationPath = dataPath(sim),
+                           rasterToMatch = sim$Raster1,
+                           studyArea = sim$rP,
+                           length = Inf, useCache = TRUE,
+                           userTags = c("objectName:Raster2", 
+                                        "fileName:CAN_NALCMS_LC_30m_LAEA_mmu12_urb05"))#,
+      # cacheId = "af8ac2b611698d718c31a8f339ad1f07")
       gc()
       
       raster::extent(sim$Raster2) <- raster::alignExtent(extent = raster::extent(sim$Raster2), object = sim$Raster1, snap = "near")
@@ -128,21 +129,21 @@ doEvent.prepTiles = function(sim, eventTime, eventType) {
                                     " (Time: "
                                     , Sys.time(), ")")))
       sim$Raster3 <- Cache(prepInputs, url = sim$urlRaster3,
-                            destinationPath = dataPath(sim),
-                            rasterToMatch = sim$Raster2,
-                            studyArea = sim$rP, length = Inf, useCache = TRUE,
-                            userTags = c("objectName:Raster3", 
-                                         "fileName:C2C_change_year"))#,
-                           # cacheId = "c61d504bbb68d83f305d8f876b311c86")
+                           destinationPath = dataPath(sim),
+                           rasterToMatch = sim$Raster2,
+                           studyArea = sim$rP, length = Inf, useCache = TRUE,
+                           userTags = c("objectName:Raster3", 
+                                        "fileName:C2C_change_year"))#,
+      # cacheId = "c61d504bbb68d83f305d8f876b311c86")
       gc()
- 
+      
       raster::extent(sim$Raster3) <- raster::alignExtent(extent = raster::extent(sim$Raster3), object = sim$Raster2, snap = "near")
       if (!raster::compareRaster(sim$Raster3, sim$Raster2, extent = TRUE)){
         stop("Rasters don't align. Please debug it.")
       }
       
       message(crayon::green("Big rasters prepared for splitting!"))
-
+      
       # schedule future event(s)
       sim <- scheduleEvent(sim, start(sim), "prepTiles", "tileRasters", eventPriority = 1)
     },
@@ -151,31 +152,32 @@ doEvent.prepTiles = function(sim, eventTime, eventType) {
       # Now all rasterd need to be tiled using the paramethers provided nx and ny
       message(crayon::yellow(paste0("Splitting Raster1 tiles", " (Time: "
                                     , Sys.time(), ")")))
-      suppressWarnings(dir.create(file.path(cachePath(sim), "Raster1")))
+      suppressWarnings(dir.create(file.path(dataPath(sim), "Raster1")))
+      browser()
       sim$Raster1 <- Cache(splitRaster, r = sim$Raster1, 
                            nx = params(sim)$prepTiles$nx, 
                            ny = params(sim)$prepTiles$ny, 
                            buffer = params(sim)$prepTiles$buffer,  # Splitting disturbanceType Raster, write to disk,
                            rType = params(sim)$prepTiles$rType, 
-                           path = file.path(cachePath(sim), "Raster1"),
+                           path = file.path(dataPath(sim), "Raster1"),
                            cacheId = "splitRaster1") # override the original in memory
       gc()
       
       message(crayon::yellow(paste0("Splitting Raster2 tiles", " (Time: "
                                     , Sys.time(), ")")))
-      suppressWarnings(dir.create(file.path(cachePath(sim), "Raster2")))
+      suppressWarnings(dir.create(file.path(dataPath(sim), "Raster2")))
       sim$Raster2 <- Cache(splitRaster, r = sim$Raster2,
                            nx = params(sim)$prepTiles$nx, 
                            ny = params(sim)$prepTiles$ny, 
                            buffer = params(sim)$prepTiles$buffer,  # Splitting landCover Raster, write to disk,
                            rType = params(sim)$prepTiles$rType,
-                           path = file.path(cachePath(sim), "Raster2"),
+                           path = file.path(dataPath(sim), "Raster2"),
                            cacheId = "splitRaster2") # override the original in memory
       gc()
       
       message(crayon::yellow(paste0("Splitting Raster3 tiles", " (Time: "
                                     , Sys.time(), ")")))
-      suppressWarnings(dir.create(file.path(cachePath(sim), "Raster3")))
+      suppressWarnings(dir.create(file.path(dataPath(sim), "Raster3")))
       sim$Raster3 <- Cache(splitRaster, r = sim$Raster3, 
                            nx = params(sim)$prepTiles$nx, 
                            ny = params(sim)$prepTiles$ny, 
@@ -184,7 +186,7 @@ doEvent.prepTiles = function(sim, eventTime, eventType) {
                            path = file.path(cachePath(sim), "Raster3"),
                            cacheId = "splitRaster3") # override the original in memory
       gc()
-
+      
       sim$rastersList <- list("Raster1" = sim$Raster1, 
                               "Raster2" = sim$Raster2, 
                               "Raster3" = sim$Raster3) # Splitted rasters' list
@@ -212,7 +214,7 @@ doEvent.prepTiles = function(sim, eventTime, eventType) {
       message(crayon::yellow(paste0("Neither its url or Raster2 were provided. Using default.",
                                     " ATTENTION: The default url for this raster downloads a .rar file.",
                                     " prepInputs doesn't deal with .rar for now. Please unrar it",
-                                    " manually once it is downloaded.")))
+                                    " manually once it is downloaded."))) # Is this really true?
       sim$urlRaster2 <- extractURL(objectName = "urlRaster2", sim = sim)
       
     }
@@ -226,82 +228,13 @@ doEvent.prepTiles = function(sim, eventTime, eventType) {
   }
   
   if (!suppliedElsewhere("rP", sim)) {
-    if (any(is.null(P(sim)$testArea), (!is.null(P(sim)$testArea) &
-                                       P(sim)$testArea == FALSE))) {
-      if (!is.null(sim$specificTestArea)) {
-        sim$rP <- NULL
-        message(crayon::yellow(paste0(
-          "Test area is FALSE or NULL, but specificTestArea is not. Ignoring 'specificTestArea' and running the analysis for the whole country. ",
-          "To set a study area, use testArea == TRUE.")))
-      } else {
-        sim$rP <- NULL
-        message(crayon::yellow("Test area is FALSE or NULL. Running the analysis for the whole country."))
-      }
-    } else {
-      if (is.null(sim$specificTestArea)) {
-        sim$polyMatrix <- matrix(c(-79.471273, 48.393518), ncol = 2) 
-        sim$areaSize <- 1e+11
-        set.seed(1234)
-        sim$rP <- randomPolygon(x = polyMatrix, area = areaSize) # Create Random polygon
-        message(crayon::yellow("Test area is TRUE, specificTestArea is 'NULL'. Cropping and masking to an area in south Ontario."))
-      } else {
-        if (sim$specificTestArea == "boreal") {
-          if (!is.null(sim$mapSubset)) {
-            if (sim$mapSubset != "Canada") {
-            sArP <- Cache(prepInputs,
-                          url = "http://www12.statcan.gc.ca/census-recensement/2011/geo/bound-limit/files-fichiers/gpr_000b11a_e.zip",
-                          targetFile = "gpr_000b11a_e.shp",
-                          # Subsetting to specific Provinces
-                          destinationPath = dataPath(sim), 
-                          userTags = "objectName:sArP") %>%
-              raster::subset(PRENAME %in% sim$mapSubset)
-              if (nrow(sArP@data) == 0) {
-                stop(paste0("There is no Canadian Province called ",
-                          sim$mapSubset,
-                          ". Please provide a Canadian province name in English for subsetMap, ",
-                          "or use 'NULL' (does not subset boreal)."))
-              }
-            } else {
-              if (sim$mapSubset == "Canada") {
-                sArP <- NULL # [ FIX ] when I can run the first rasters again! So far, it is complicated to do it cause it is being used elsewhere
-                  # Cache(raster::getData('GADM', country = 'CAN', level = 0), 
-                  #             userTags = "Canada.sArP")
-              }
-            }
-          } else {
-              sArP <- NULL
-            }
-          message(crayon::yellow("Test area is TRUE. Cropping and masking to the Canadian Boreal."))
-          sim$rP <- prepInputs(alsoExtract = "similar", # [ FIX ] Needs a URL to make it more reproducible!
-                               archive = file.path(dataPath(sim), "BRANDT_OUTLINE_Dissolve.zip"),
-                               targetFile = file.path(dataPath(sim), "BRANDT_OUTLINE_Dissolve.shp"),
-                               studyArea = sArP,
-                               destinationPath = dataPath(sim))
-        } else {
-          if (!is.null(sim$specificTestArea)) {
-            sim$rP <- Cache(prepInputs,
-                            url = "http://www12.statcan.gc.ca/census-recensement/2011/geo/bound-limit/files-fichiers/gpr_000b11a_e.zip",
-                            targetFile = "gpr_000b11a_e.shp",
-                            # Subsetting to a specific Province
-                            archive = "gpr_000b11a_e.zip",
-                            destinationPath = dataPath(sim),
-                            userTags = "objectName:rP") %>%
-              raster::subset(PRENAME == sim$specificTestArea)
-            if (nrow(sim$rP@data) == 0) {
-              stop(paste0("There is no Canadian Province called ",
-                          sim$specificTestArea,
-                          ". Please provide a Canadian province name in English for specificTestArea, ",
-                          "use 'boreal', or use 'NULL' (creates a random area in South Ontario)."))
-            } else {
-              message(crayon::yellow(paste0("Test area is TRUE. Cropped and masked to ",
-                                            sim$specificTestArea)))
-              
-            }
-          }
-        }
-      }
-    }
+    sim$rP <- defineStudyArea(testArea = P(sim)$testArea, 
+                              specificTestArea = sim$specificTestArea, 
+                              mapSubset = sim$mapSubset, 
+                              destinationFolder = dataPath(sim))
+  }
+    
+    return(invisible(sim))
   }
   
-  return(invisible(sim))
-}
+  
