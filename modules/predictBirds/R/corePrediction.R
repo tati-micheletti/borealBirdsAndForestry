1a3605ef66043483c4b0408f38dabf0bf3eba2e6
Tati Micheletti <tati.micheletti@gmail.com>
2019-05-02 22:13:14 +0000

    changes to corePrediction work: still rasters

--- modules/predictBirds/R/corePrediction.R
+++ modules/predictBirds/R/corePrediction.R
@@ -1,4 +1,5 @@
 corePrediction <- function(bird, model, birdDensityRas,
+                           predictedName,
                            disturbanceRas = NULL,
                            successionLayers = NULL,
                            staticLayers = NULL,
@@ -9,7 +10,6 @@ corePrediction <- function(bird, model, birdDensityRas,
   successionLayersNames <- names(successionLayers)
   staticLayersNames <- names(staticLayers)
   message(crayon::yellow(paste0("Predicting for ", bird , ". Prediction for time ", currentTime)))
-  
   if ("glmerMod" %in% class(model)){
     nameStackRas1 <- names(model@frame)[2]
     nameStackRas2 <- names(model@frame)[3]
@@ -25,7 +25,6 @@ corePrediction <- function(bird, model, birdDensityRas,
         }, error = function(e){
           stop("crs and or extents don't align. Check you layers have the same crs and projection before this call")
         })
-        predictedName <- file.path(pathData, paste0("predicted/predicted", bird, "Year", currentTime, ".tif"))
         if (isTRUE(overwritePredictions)||!file.exists(predictedName)){
           message(crayon::yellow(paste0(" Starting prediction raster for ", bird, ". This might take some time... [", Sys.time(),"]")))
           startTime <- Sys.time()
@@ -53,7 +52,7 @@ corePrediction <- function(bird, model, birdDensityRas,
     }
   }
   focDis <- as.numeric(gsub("[^\\d]+", "", nameStackRas1, perl=TRUE))
-  predictedName <- file.path(pathData, paste0("predicted/predictedFocal", focDis, "m", bird, currentTime, ".tif"))
+  
   if (isTRUE(overwritePredictions)||!file.exists(predictedName)){
     valsD <- log(raster::getValues(birdDensityRas)) # log the value of densities so it is the same of the original model
     valsD[valsD < -0.99] <- -1
@@ -61,20 +60,35 @@ corePrediction <- function(bird, model, birdDensityRas,
     rm(valsD)
     gc()
     tryCatch({
-      stkLays <- raster::stack(disturbanceRas, birdDensityRas) # Might need to remove individual rasters here
+      stkLays <- raster::stack(disturbanceRas, birdDensityRas)
     }, error = function(e){
-      stop("Please make sure the disturbanceRaster has the same crs, projection and alignment of the birdDensityRaster")
+      tryCatch({
+        problem <- if (unique(raster::res(birdDensityRas[[1]])) != unique(raster::res(disturbanceRas))) 
+          " different resolution" else if (raster::extent(birdDensityRas[[1]]) != raster::extent(disturbanceRas)) 
+            " different extents" else " possibly alignment"
+        message("At least one birdDensityRaster can't be stacked with disturbanceRaster. The probably problem is: ", problem)
+        warning("The density raster used has originally a resolution of ", raster::res(birdDensityRas[[1]]), 
+                "")
+        birdDensityRas <- reproducible::postProcess(birdDensityRas, rasterToMatch = disturbanceRas, 
+                                                    destinationPath = tempdir(), filename2 = NULL)
+        raster::extent(birdDensityRas) <- raster::alignExtent(extent = raster::extent(birdDensityRas),
+                                                   object = disturbanceRas,
+                                                   snap = "near")
+        stkLays <- raster::stack(disturbanceRas, birdDensityRas)
+      }, error = function(e){
+        stop("Please make sure the disturbanceRaster has the same crs, projection and alignment of the birdDensityRaster")
+      })
     })
     names(stkLays)[1] <- nameStackRas1
     names(stkLays)[2] <- nameStackRas2
-    suppressWarnings(predicted <- fitModel(inRas = stkLays, 
+    predicted <- suppressWarnings(fitModel(inRas = stkLays, 
                                            inputModel = model, 
                                            x = bird, 
                                            tileYear = currentTime))
+  } else {
+    message("Prediction already exists for ", bird, ". Returning ", predictedName)
+    predicted <- raster(predictedName)
   }
-  predicted <- raster(predicted)
-  names(predicted) <- paste0("predictedFocal", focDis, "m", bird, currentTime)
   gc()
-  browser()
   return(predicted) # The predicted value is NOT multiplied by 1000! For that, need to change fitModel!
 }
