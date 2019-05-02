1fb7ea2e4eaac4cb7555e3f34cb486f2d1b01f26
Tati Micheletti <tati.micheletti@gmail.com>
2019-05-02 22:31:01 +0000

    small fixes on arguments

--- modules/birdDensityBCR_Prov_LCC/R/maskingSpeciesRange.R
+++ modules/birdDensityBCR_Prov_LCC/R/maskingSpeciesRange.R
@@ -4,6 +4,7 @@ maskingSpeciesRange <- function(densityRasters = spRas,
                       filenameRas = densityFiles[[sp]],
                       birdsRangeList = birdsRangeList){
   
+  if (!file.exists(filenameRas)){
       pathHR <- file.path(pathData, "maskedHR") %>%
         checkPath(create = TRUE)
       spRangePath <- birdsRangeList[[sp]]
@@ -16,25 +17,19 @@ maskingSpeciesRange <- function(densityRasters = spRas,
         spRange <- raster::raster(file.path(pathHR, paste0(sp, "_corecurrmeanBSI.asc")))
       }
       crs(spRange) <- "+proj=lcc +lat_1=49 +lat_2=77 +lat_0=0 +lon_0=-95 +x_0=0 +y_0=0 +ellps=GRS80 +units=m +no_defs" #Had to "guess" the crs, its not in the original files!
-      if (!file.exists(file.path(pathData, paste0(sp, "unmasked.tif")))){
-        spRange <- postProcess(spRange, studyArea = studyArea, format = "GTiff", datatype = "INT1U", filename2 = paste0(sp, "unmasked.tif"), 
+        spRange <- postProcess(spRange, studyArea = studyArea, format = "GTiff", 
+                               datatype = "INT1U", rasterToMatch = densityRasters, 
+                               filename2 = NULL,
                                destinationPath = pathData)
-      } else {
-        spRange <- raster::raster(file.path(pathData, paste0(sp, "unmasked.tif")))
-      }
-      vals <- raster::getValues(x = spRange)
-      vals[vals < 1] <- NA # Masking to ones. Binary raster
-      spRange <- raster::setValues(x = spRange, values = vals)
-      rm(vals)
-      invisible(gc())
-      if (!file.exists(filenameRas)){
-        spRange <- projectInputs(spRange, targetCRS = crs(densityRasters))# Need to reproject the SpRange as I want the same projection as the original trends raster in the end
-        suppressWarnings(reproducible::postProcess(x = densityRasters, rasterToMatch = spRange,
-                                                   maskWithRTM = TRUE, format = "GTiff", overwrite = TRUE, 
-                                                   filename2 = filenameRas, useCache = FALSE)) 
-      } else {
-        spRange <- filenameRas
-      }
-      invisible(gc())
+        vals <- raster::getValues(spRange)
+        vals[vals < 1] <- NA
+        spRange <- raster::setValues(x = spRange, values = vals)
+        rm(vals)
+        invisible(gc())
+      reproducible::postProcess(x = densityRasters, rasterToMatch = spRange,
+                                                   maskWithRTM = TRUE, format = "GTiff", overwrite = TRUE,
+                                                   filename2 = filenameRas, useCache = FALSE)
+  }
+  invisible(gc())
   return(filenameRas)
 }
--- modules/predictBirds/R/fitModel.R
+++ modules/predictBirds/R/fitModel.R
@@ -1,7 +1,7 @@
-fitModel <- function(inRas = stack, 
-                     inputModel = models, 
-                     spName = spName,
-                     tileYear = currentTime){
+fitModel <- function(inRas, 
+                     inputModel, 
+                     x,
+                     tileYear){
 
   #Raster predict will work with any model that works with 'predict'
   if ("glmerMod" %in% class(inputModel)){
@@ -9,28 +9,28 @@ fitModel <- function(inRas = stack,
                           model = inputModel,
                           re.form = NA,
                           type = "response") # re.form = NA drops the random effects from the models; "response" gives us the density, not log
-    names(prediction) <- paste0("prediction", spName, tileYear)
+    names(prediction) <- paste0("prediction", x, tileYear)
   } else {
     if ("glm" %in% class(inputModel)){
       tempRas <- raster::raster(nrows = nrow(inRas[[1]]), ncols = ncol(inRas[[1]]), 
                                 crs = raster::crs(inRas[[1]]), ext = extent(inRas[[1]]), 
                                 resolution = res(inRas[[1]]), vals = 0)
       off <- names(inputModel$data)[grepl(pattern = "OF", x = names(inputModel$data)) & 
-                                    grepl(pattern = spName, x = names(inputModel$data))]
+                                    grepl(pattern = x, x = names(inputModel$data))]
       names(tempRas) <- off
       assign(x = off, value = tempRas)
       inRas <- addLayer(x = inRas, get(off))
       prediction <- predict(object = inRas,
                             model = inputModel,
                             type = "response") # No re.form in glm
-      names(prediction) <- paste0("prediction", spName, tileYear)
+      names(prediction) <- paste0("prediction", x, tileYear)
     }
   }
   # I will try without first... If it doesn't fir, we multiply
   #prediction[] <- prediction[]*1000 # Results are already in density scale and multiplied by 1000 for saving space
   #suppressWarnings(storage.mode(prediction[]) <- "integer")
   
-  message(crayon::green(paste0(spName, " prediction finalized for ", tileYear)))
+  message(crayon::green(paste0(x, " prediction finalized for year ", tileYear)))
   
   return(prediction)
 }
