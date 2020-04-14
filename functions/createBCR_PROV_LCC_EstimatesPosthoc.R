32bd5756312e1c8df08c7aaa208609778d6aa4db
Tati Micheletti <tati.micheletti@gmail.com>
2020-04-13 19:37:34 -0700

    Upgraded funs

--- functions/createBCR_PROV_LCC_EstimatesPosthoc.R
+++ functions/createBCR_PROV_LCC_EstimatesPosthoc.R
@@ -13,26 +13,48 @@ createBCR_PROV_LCC_EstimatesPosthoc <- function(BCR, pathToDSave,
   BCRsf <- BCRsf[BCRsf$COUNTRY == "CANADA",] # Excluding the USA from the analysis
   rasBCR <- fasterize::fasterize(sf = BCRsf, raster = LCC05, field = "BCR")
   rasBCR[] <- rasBCR[]
-  rasBCR <- Cache(postProcess, rasBCR, rasterToMatch = raster(densityMap), filename2 = file.path(pathToDSave, "RTMedBCR_posthoc"), format = "GTiff")
+  rasBCR <- Cache(postProcess, x = rasBCR, destinationPath = pathToDSave, 
+                  rasterToMatch = raster(densityMap), 
+                  filename2 = NULL,  
+                  format = "GTiff")
   
   # # ==========+ PROV
   message(crayon::yellow(paste0("Fasterizing PROV")))
   PROV_ID <- data.table(PROVINCE_S = unique(BCRsf$PROVINCE_S), ID = seq(200, 199 + length(unique(BCRsf$PROVINCE_S))))
   BCRsf$PROV_ID <- PROV_ID$ID[match(BCRsf$PROVINCE_S, PROV_ID$PROVINCE_S)]
   rasPROV <- fasterize::fasterize(sf = BCRsf, raster = LCC05, field = "PROV_ID")
-  rasPROV <- Cache(postProcess, rasPROV, rasterToMatch = raster(densityMap), filename2 = file.path(pathToDSave, "RTMedPROV_posthoc"), format = "GTiff")
+  rasPROV <- Cache(postProcess, rasPROV, rasterToMatch = raster(densityMap),
+                   destinationPath = pathToDSave,
+                   filename2 = NULL, 
+                   format = "GTiff")
   
   # 3. Create a lookout table
-  PROVabb <- structure(list(PRENAME = structure(1:13, .Label = c("Alberta", 
-                                                                 "British Columbia", "Manitoba", "New Brunswick", "Newfoundland and Labrador", 
-                                                                 "Northwest Territories", "Nova Scotia", "Nunavut", "Ontario", 
-                                                                 "Prince Edward Island", "Quebec", "Saskatchewan", "Yukon"), class = "factor"), 
+  PROVabb <- structure(list(PRENAME = structure(1:13, 
+                                                .Label = c("Alberta",
+                                                           "British Columbia",
+                                                           "Manitoba",
+                                                           "New Brunswick", 
+                                                           "Newfoundland and Labrador",
+                                                           "Northwest Territories", 
+                                                           "Nova Scotia", 
+                                                           "Nunavut", 
+                                                           "Ontario",
+                                                           "Prince Edward Island", 
+                                                           "Quebec", 
+                                                           "Saskatchewan", 
+                                                           "Yukon"), 
+                                                class = "factor"), 
                             ID = structure(c(207L, 206L, 200L, 210L, 201L, 202L, 211L, 203L, 209L, 212L, 
                                              204L, 208L, 205L), class = "numeric"), 
                             PROV = structure(c(1L, 2L, 3L, 4L, 5L, 7L, 6L, 8L, 9L, 10L, 
-                                               11L, 12L, 13L), .Label = c("AB", "BC", "MB", "NB", "NL", 
-                                                                          "NS", "NT", "NU", "ON", "PE", "QC", "SK", "YT"), class = "factor")), 
-                       .Names = c("PRENAME", "ID", "PROV"), row.names = c(NA, -13L), class = "data.frame")
+                                               11L, 12L, 13L), .Label = c("AB", "BC", "MB", 
+                                                                          "NB", "NL", "NS", 
+                                                                          "NT", "NU", "ON", 
+                                                                          "PE", "QC", "SK", 
+                                                                          "YT"), 
+                                             class = "factor")), 
+                       .Names = c("PRENAME", "ID", "PROV"), row.names = c(NA, -13L), 
+                       class = "data.frame")
   
   if (justBCRProvLCC){
     lccProvBCR <- raster::stack(LCC05, rasPROV, rasBCR)
